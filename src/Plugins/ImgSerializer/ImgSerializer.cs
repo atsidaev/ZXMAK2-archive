db4903e001565c97c15efba04228bd0a4134a160
Alex Makeev <zxmak@mail.ru>
2012-05-19 17:48:52 +0000

    add sprinter plugins (written by zx.pk.ru\???????)

---
+++ src/Plugins/ImgSerializer/ImgSerializer.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Text;
+using System.IO;
+using ZXMAK2.Engine.Devices.Disk;
+using ZXMAK2.Engine.Serializers;
+
+namespace DiskSerializer
+{
+    public class ImgSerializer:FormatSerializer
+    {
+        private DiskImage _diskImage;
+        private byte[] il = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+        public ImgSerializer(DiskImage diskImage)
+        {
+            this._diskImage = diskImage;
+            //_diskImage.Init
+        }
+
+        public override void Deserialize(Stream stream)
+        {
+            this.loadFromStream(stream);
+            this._diskImage.ModifyFlag = ModifyFlag.None;
+            this._diskImage.Present = true;
+        }
+
+        public void format_HD()
+        {
+            for (int i = 0; i < this._diskImage.CylynderCount; i++)
+            {
+//                Track[] trackArray = this._diskImage this._cylynderList[i];
+                
+                for (int m = 0; m < _diskImage.SideCount; m++)
+                {
+                    ArrayList sectorList = new ArrayList();
+                    for (int n = 0; n < 0x12; n++)
+                    {
+                        SimpleSector sector = new SimpleSector(i, m, this.il[n], 2, new byte[0x200]);
+                        for (int num4 = 0; num4 < 0x200; num4++)
+                        {
+                            sector.Data[num4] = 0;
+                        }
+                        sector.SetAdCrc(true);
+                        sector.SetDataCrc(true);
+                        sectorList.Add(sector);
+                    }
+                    
+
+                    _diskImage.GetTrackImage(i, m).AssignSectors(sectorList);
+//                    trackArray[m].AssignSectors(sectorList);
+                }
+            }
+            _diskImage.ModifyFlag = ZXMAK2.Engine.Devices.Disk.ModifyFlag.None;
+        }
+
+
+        private void loadFromStream(Stream stream)
+        {
+            int cylynderCount = ((int)stream.Length) / 0x4800;
+//            int ti;
+            if ((stream.Length % 0x4800L) > 0L)
+            {
+                this._diskImage.SetPhysics(cylynderCount + 1, 2);
+            }
+            else
+            {
+                this._diskImage.SetPhysics(cylynderCount, 2);
+            }
+            //this._diskImage.format_trdos();
+            //this._diskImage.Format(); //?
+            //this._diskImage.SetPhysics(80, 2);
+//            this._diskImage.GetTrackImage(0,0).AssignSectors(
+            //this.format_HD();
+            for (int i = 0; i < this._diskImage.CylynderCount; i++)
+            {
+                for (int m = 0; m < _diskImage.SideCount; m++)
+                {
+                    ArrayList sectorList = new ArrayList();
+                    for (int n = 0; n < 0x12; n++)
+                    {
+                        SimpleSector sector = new SimpleSector(i, m, this.il[n], 2, new byte[0x200]);
+//                        byte[] buffer = new byte[0x200];
+                        stream.Read(sector.Data, 0, 0x200);
+//                        sector.Data = buffer;
+                        sector.SetAdCrc(true);
+                        sector.SetDataCrc(true);
+                        sectorList.Add(sector);
+                    }
+                    _diskImage.GetTrackImage(i, m).AssignSectors(sectorList);
+                    //_diskImage.GetTrackImage(i, m).
+                }
+            }
+            _diskImage.ModifyFlag = ZXMAK2.Engine.Devices.Disk.ModifyFlag.None;
+
+/*            for (int i = 0; stream.Position < stream.Length; i += 0x200)
+            {
+                byte[] buffer = new byte[0x200];
+                stream.Read(buffer, 0, 0x200);
+                //this._diskImage.writeLogicalSector(i >> 13, (i >> 12) & 1, ((i >> 8) & 15) + 1, buffer);
+                ti = i >> 9;
+                //this._diskImage.writeLogicalSector(ti / 36, (ti / 18) & 1, (ti - ((ti / 18) * 18)) + 1,buffer);
+                this._diskImage.writeLogicalSector(ti / 36, (ti / 18) & 1, (ti % 18) + 1, buffer);
+
+            }*/
+        }
+
+        private void saveToStream(Stream stream)
+        {
+            int ti;
+            for (int i = 0; i < (0x4800 * this._diskImage.CylynderCount); i += 0x200)
+            {
+                byte[] buffer = new byte[0x200];
+                //this._diskImage.readLogicalSector(i >> 13, (i >> 12) & 1, ((i >> 8) & 15) + 1, buffer);
+                ti = i >> 9;
+                this._diskImage.readLogicalSector(ti / 36, (ti / 18) & 1, (ti % 18) + 1, buffer);
+                //this._diskImage.readLogicalSector(ti / 36, (ti / 18) & 1, (ti - ((ti / 18) * 18)) + 1, buffer);
+                stream.Write(buffer, 0, 0x200);
+            }
+        }
+
+        public override void Serialize(Stream stream)
+        {
+            this.saveToStream(stream);
+            this._diskImage.ModifyFlag = ModifyFlag.None;
+        }
+
+        public override void SetReadOnly(bool readOnly)
+        {
+            this._diskImage.IsWP = readOnly;
+        }
+
+        public override void SetSource(string fileName)
+        {
+            this._diskImage.FileName = fileName;
+        }
+
+        public override bool CanDeserialize
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override bool CanSerialize
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override string FormatExtension
+        {
+            get
+            {
+                return "IMG";
+            }
+        }
+
+        public override string FormatGroup
+        {
+            get
+            {
+                return "Disk images";
+            }
+        }
+
+        public override string FormatName
+        {
+            get
+            {
+                return "IMG disk image";
+            }
+        }
+
+
+
+    }
+}
---
+++ src/Plugins/Sprinter/Keyboard/KeyboardSprinter.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using Microsoft.DirectX.DirectInput;
+using ZXMAK2.Engine.Interfaces;
+using ZXMAK2.Engine.Devices;
+
+namespace Plugins.Keyboard
+{
+    class KeyboardSprinter :  IKeyboardDevice, IBusDevice
+    {
+        private int m_busOrder;
+        private IKeyboardState m_keyboardState;
+        private long m_intState;
+//        private Microsoft.DirectX.DirectInput.Device m_keyb;
+        private KeyboardDevice m_zxkbd;
+
+        private Key[] m_kbd_sc_num = {
+                                         Key.Escape,
+                                         Key.F1,
+                                         Key.F2,
+                                         Key.F3,
+                                         Key.F4,
+                                         Key.F5,
+                                         Key.F6,
+                                         Key.F7,
+                                         Key.F8,
+                                         Key.F9,
+                                         Key.F10,
+                                         Key.Delete
+                                     };
+
+        private byte[][] m_kbd_scancodes = new byte[][] {
+                                               new byte[]{0x76,0x00}, //Esc
+                                               new byte[]{0x05,0x00}, //F1
+                                               new byte[]{0x06,0x00}, //F2
+                                               new byte[]{0x04,0x00}, //F3
+                                               new byte[]{0x0c,0x00}, //F4
+                                               new byte[]{0x03,0x00}, //F5
+                                               new byte[]{0x0B,0x00}, //F6
+                                               new byte[]{0x83,0x00}, //F7
+                                               new byte[]{0x0A,0x00}, //F8
+                                               new byte[]{0x01,0x00}, //F9
+                                               new byte[]{0x09,0x00}, //F10
+                                               new byte[]{0xE0,0x71} //Del
+                                            };
+
+        private byte[][] m_kbd_buff = new byte[16][];
+
+        public KeyboardSprinter()
+        {
+            for (int i = 0; i<m_kbd_buff.Length;i++)
+                m_kbd_buff[i] = new byte[0x04];
+        }
+
+        public void BusConnect()
+        {
+        }
+
+        public void BusDisconnect()
+        {
+        }
+
+        public void BusInit(IBusManager bmgr)
+        {
+
+//            bmgr.SubscribeRDIO(1, 0, new BusReadIoProc(this.readPortFE));
+            m_zxkbd = bmgr.FindDevice(typeof(KeyboardDevice)) as KeyboardDevice;
+            if (m_zxkbd == null) throw new ApplicationException("Standart ZX Keyboard Device not found");
+            //InitializeKeyboard();
+            bmgr.SubscribeEndFrame(new BusFrameEventHandler(ScanKeys));
+        }
+
+        public int BusOrder
+        {
+            get
+            {
+                return this.m_busOrder;
+            }
+            set
+            {
+                this.m_busOrder = value;
+            }
+        }
+
+        public BusCategory Category
+        {
+            get
+            {
+                return BusCategory.Keyboard;
+            }
+        }
+
+        public IKeyboardState KeyboardState
+        {
+            get
+            {
+                return this.m_keyboardState;
+            }
+            set
+            {
+                this.m_keyboardState = value;
+
+//                this.m_intState = scanState(this.m_keyboardState);
+            }
+        }
+
+/*        public void InitializeKeyboard()
+        {
+            m_keyb = new Microsoft.DirectX.DirectInput.Device(SystemGuid.Keyboard);
+            m_keyb.SetCooperativeLevel(null , CooperativeLevelFlags.Background | CooperativeLevelFlags.NonExclusive);
+            m_keyb.Acquire();
+        }*/
+
+        private void ScanKeys()
+        {
+            for (int num = 0;num<m_kbd_sc_num.Length;num++)
+            {                
+                if (m_keyboardState[m_kbd_sc_num[num]]){
+
+                }
+
+            }
+            
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Sprinter AT Keyboard";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Sprinter AT Keyboard";
+            }
+        }
+
+    }
+}
