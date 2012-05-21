3276b7b53e41a05211509717db7a2d5e56cc12ac
Alex Makeev <zxmak@mail.ru>
2012-05-21 23:54:41 +0000

    use bit 6 for zebest ULA, according to zebest request

--- ZXMAK2/Engine/Devices/Ula/ZebestUla.cs
+++ ZXMAK2/Engine/Devices/Ula/ZebestUla.cs
@@ -7,14 +7,25 @@ namespace ZXMAK2.Engine.Devices.Ula
     public class ZebestUla : UlaPentagon
     {
         public override string Name { get { return "Zebest ULA (Pentagon+BRIGHT)"; } }
-        public override string Description { get { return "ULA device based on Pentagon + border bright mod\nbit 7 of port #FE = inversed bright bit for border"; } }
+        public override string Description { get { return "ULA device based on Pentagon + border bright mod\nbit 6 of port #FE = bright bit for border"; } }
+
+        public ZebestUla()
+        {
+            c_ulaBorderTop = 64;
+            c_ulaBorderBottom = 48;
+            c_ulaBorderLeftT = 28;
+            c_ulaBorderRightT = 28;
+
+            c_ulaWidth = (c_ulaBorderLeftT + 128 + c_ulaBorderRightT) * 2;
+            c_ulaHeight = (c_ulaBorderTop + 192 + c_ulaBorderBottom);
+        }
 
         public override byte PortFE
         {
             set
             {
                 base.PortFE = value;
-                int borderAttr = (value & 7) | (((value & 0x80) /*^ 0x80*/) >> 4);
+                int borderAttr = (value & 7) | ((value & 0x40 ) >> 3);
                 _borderColor = Palette[borderAttr];
             }
         }
