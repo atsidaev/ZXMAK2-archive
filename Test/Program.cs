abec053f1f72d8ee4f42212a9bb74b8d64d24301
Alex Makeev <zxmak@mail.ru>
2012-05-24 23:35:02 +0000

    fix ULA48 contention; add sanity test for 48 early model

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,155 +9,164 @@ using ZXMAK2.Engine.Interfaces;
 
 namespace Test
 {
-	public class Program
-	{
-		static void Main(string[] args)
-		{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
             if (args.Length >= 1 && args[0].ToLower() == "/zexall")
             {
                 runZexall();
                 return;
             }
 
-			SanityUla("NOP      ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum48(), new byte[] { 0x00 }, s_patternUla48_NOP);
-			SanityUla("NOP      ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x00 }, s_patternUla128_NOP);
-			SanityUla("INC HL   ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x23 }, s_patternUla128_INCHL);
-			SanityUla("LD A,(HL)", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x7E }, s_patternUla128_LDA_HL_);
-			SanityUla("LD (HL),A", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x77 }, s_patternUla128_LDA_HL_); // pattern the same as ld a,(hl)
-			SanityUla("OUT (C),A", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0xED, 0x79, 0x03 }, s_patternUla128_OUTCA);
-			SanityUla("IN A,(C) ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0xED, 0x78, 0x03 }, s_patternUla128_OUTCA); // pattern the same as out (c),a
+            SanityUla("NOP      ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum48(), new byte[] { 0x00 }, s_patternUla48_NOP);
+            SanityUla("NOP      ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum48_Early(), new byte[] { 0x00 }, s_patternUla48_Early_NOP);
+            SanityUla("NOP      ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x00 }, s_patternUla128_NOP);
+            SanityUla("INC HL   ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x23 }, s_patternUla128_INCHL);
+            SanityUla("LD A,(HL)", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x7E }, s_patternUla128_LDA_HL_);
+            SanityUla("LD (HL),A", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0x77 }, s_patternUla128_LDA_HL_); // pattern the same as ld a,(hl)
+            SanityUla("OUT (C),A", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0xED, 0x79, 0x03 }, s_patternUla128_OUTCA);
+            SanityUla("IN A,(C) ", new ZXMAK2.Engine.Devices.Ula.UlaSpectrum128(), new byte[] { 0xED, 0x78, 0x03 }, s_patternUla128_OUTCA); // pattern the same as out (c),a
 
-			Console.WriteLine("=====Engine Performance Benchmark (500 frames rendering time)=====");
-			Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.AboveNormal;
-			int frameCount = 50 * 10;
-			ExecTests("testVideo.z80", frameCount);
-			ExecTests("testVideo.z80", frameCount);
-			ExecTests("testVideo.z80", frameCount);
-			ExecTests("zexall.sna", frameCount);
-			ExecTests("zexall.sna", frameCount);
-			ExecTests("zexall.sna", frameCount);
-		}
+            Console.WriteLine("=====Engine Performance Benchmark (500 frames rendering time)=====");
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.AboveNormal;
+            int frameCount = 50 * 10;
+            ExecTests("testVideo.z80", frameCount);
+            ExecTests("testVideo.z80", frameCount);
+            ExecTests("testVideo.z80", frameCount);
+            ExecTests("zexall.sna", frameCount);
+            ExecTests("zexall.sna", frameCount);
+            ExecTests("zexall.sna", frameCount);
+        }
 
-		private static void SanityUla(string name, IUlaDevice ula, byte[] opcode, int[] pattern)
-		{
-			IMemoryDevice mem = new ZXMAK2.Engine.Devices.Memory.MemoryPentagon128();
-			SpectrumConcrete p128 = new SpectrumConcrete();
-			p128.Init();
-			p128.BusManager.Disconnect();
-			p128.BusManager.Clear(); 
-			p128.BusManager.Add((BusDeviceBase)mem);
-			p128.BusManager.Add((BusDeviceBase)ula);
-			p128.BusManager.Connect();
-			p128.IsRunning = true;
-			p128.DoReset();
-			p128.ExecuteFrame();
-			p128.IsRunning = false;
-		 	
-			ushort offset = 0x4000;
-			for (int i = 0; i < pattern.Length; i++)
-			{
-				for(int j=0; j < opcode.Length; j++)
-					mem.WRMEM_DBG(offset++, opcode[j]);
-			}
-			p128.CPU.regs.PC = 0x4000;
-			p128.CPU.regs.IR = 0x4000;
-			p128.CPU.regs.SP = 0x4000;
-			p128.CPU.regs.AF = 0x4000;
-			p128.CPU.regs.HL = 0x4000;
-			p128.CPU.regs.DE = 0x4000;
-			p128.CPU.regs.BC = 0x4000;
-			p128.CPU.regs.IX = 0x4000;
-			p128.CPU.regs.IY = 0x4000;
-			p128.CPU.regs._AF = 0x4000;
-			p128.CPU.regs._HL = 0x4000;
-			p128.CPU.regs._DE = 0x4000;
-			p128.CPU.regs._BC = 0x4000;
-			p128.CPU.regs.MW = 0x4000;
-			p128.CPU.IFF1 = p128.CPU.IFF2 = false;
-			p128.CPU.IM = 2;
-			p128.CPU.BINT = false;
-			p128.CPU.FX = ZXMAK2.Engine.Z80.OPFX.NONE;
-			p128.CPU.XFX = ZXMAK2.Engine.Z80.OPXFX.NONE;
-			
-			long needsTact = pattern[0];
-			long frameTact = p128.CPU.Tact % ula.FrameTactCount;
-			long deltaTact = needsTact-frameTact;
-			if(deltaTact < 0)
-				deltaTact += ula.FrameTactCount;
-			p128.CPU.Tact += deltaTact;
+        private static void SanityUla(string name, IUlaDevice ula, byte[] opcode, int[] pattern)
+        {
+            IMemoryDevice mem = new ZXMAK2.Engine.Devices.Memory.MemoryPentagon128();
+            SpectrumConcrete p128 = new SpectrumConcrete();
+            p128.Init();
+            p128.BusManager.Disconnect();
+            p128.BusManager.Clear();
+            p128.BusManager.Add((BusDeviceBase)mem);
+            p128.BusManager.Add((BusDeviceBase)ula);
+            p128.BusManager.Connect();
+            p128.IsRunning = true;
+            p128.DoReset();
+            p128.ExecuteFrame();
+            p128.IsRunning = false;
 
-			for (int i = 0; i < pattern.Length-1; i++)
-			{
-				p128.DoStepInto();
-				frameTact = p128.CPU.Tact % ula.FrameTactCount;
-				if (frameTact != pattern[i + 1])
-				{
-					ConsoleColor tmp = Console.ForegroundColor;
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine(
-						"Sanity ULA {0} [{1}]:\tfailed @ {2}->{3} (should be {2}->{4})",
-						ula.GetType().Name,
-						name,
-						pattern[i],
-						frameTact,
-						pattern[i + 1]);
-					Console.ForegroundColor = tmp;
-					return;
-				}
-			}
-			ConsoleColor tmp2 = Console.ForegroundColor;
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Sanity ULA {0} [{1}]:\tpassed", ula.GetType().Name, name);
-			Console.ForegroundColor = tmp2;
-		}
+            ushort offset = 0x4000;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                for (int j = 0; j < opcode.Length; j++)
+                    mem.WRMEM_DBG(offset++, opcode[j]);
+            }
+            p128.CPU.regs.PC = 0x4000;
+            p128.CPU.regs.IR = 0x4000;
+            p128.CPU.regs.SP = 0x4000;
+            p128.CPU.regs.AF = 0x4000;
+            p128.CPU.regs.HL = 0x4000;
+            p128.CPU.regs.DE = 0x4000;
+            p128.CPU.regs.BC = 0x4000;
+            p128.CPU.regs.IX = 0x4000;
+            p128.CPU.regs.IY = 0x4000;
+            p128.CPU.regs._AF = 0x4000;
+            p128.CPU.regs._HL = 0x4000;
+            p128.CPU.regs._DE = 0x4000;
+            p128.CPU.regs._BC = 0x4000;
+            p128.CPU.regs.MW = 0x4000;
+            p128.CPU.IFF1 = p128.CPU.IFF2 = false;
+            p128.CPU.IM = 2;
+            p128.CPU.BINT = false;
+            p128.CPU.FX = ZXMAK2.Engine.Z80.OPFX.NONE;
+            p128.CPU.XFX = ZXMAK2.Engine.Z80.OPXFX.NONE;
 
-		#region ULA PATTERNS
+            long needsTact = pattern[0];
+            long frameTact = p128.CPU.Tact % ula.FrameTactCount;
+            long deltaTact = needsTact - frameTact;
+            if (deltaTact < 0)
+                deltaTact += ula.FrameTactCount;
+            p128.CPU.Tact += deltaTact;
 
-		private static int[] s_patternUla48_NOP = new int[]
-		{
-			14332, 
-			14336, 14346, 14354, 14362, 14370, 14378, 14386, 14394, 14402, 14410, 14418, 14426, 14434, 14442, 14450, 14458, 14466,
-			14470, 14474, 14478, 14482, 14486, 14490, 14494, 14498, 14502, 14506, 14510, 14514, 14518, 14522, 14526, 14530, 14534, 14538, 14542, 14546, 14550, 14554, 14558, 14562, 14570, 
-			14578, 14586, 14594, 14602, 14610, 14618, 14626, 14634, 14642, 14650, 14658, 14666, 14674, 14682, 14690, 
-			14694, 14698, 14702, 14706, 14710, 14714, 14718, 14722, 14726, 14730,
-		};
+            for (int i = 0; i < pattern.Length - 1; i++)
+            {
+                p128.DoStepInto();
+                frameTact = p128.CPU.Tact % ula.FrameTactCount;
+                if (frameTact != pattern[i + 1])
+                {
+                    ConsoleColor tmp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(
+                        "Sanity ULA {0} [{1}]:\tfailed @ {2}->{3} (should be {2}->{4})",
+                        ula.GetType().Name,
+                        name,
+                        pattern[i],
+                        frameTact,
+                        pattern[i + 1]);
+                    Console.ForegroundColor = tmp;
+                    return;
+                }
+            }
+            ConsoleColor tmp2 = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sanity ULA {0} [{1}]:\tpassed", ula.GetType().Name, name);
+            Console.ForegroundColor = tmp2;
+        }
 
-		private static int[] s_patternUla128_NOP = new int[]
-		{
+        #region ULA PATTERNS
+
+        private static int[] s_patternUla48_NOP = new int[]
+        {
+            14300, 14304, 14308, 14312, 14316, 14320, 14324, 14328, 14332, 
+            14336, 14346, 14354, 14362, 14370, 14378, 14386, 14394, 14402, 14410, 14418, 14426, 14434, 14442, 14450, 14458, 14466, 14470, 14474, 14478, 14482, 14486, 14490, 14494, 14498, 
+            14502, 14506, 14510, 14514, 14518, 14522, 14526, 14530, 14534, 14538, 14542, 14546, 14550, 14554, 14558, 14562, 14570, 14578, 14586, 14594, 14602, 14610, 14618, 14626, 14634,
+            14642, 14650, 14658, 14666, 14674, 14682, 14690, 14694, 14698, 14702, 14706, 14710, 14714, 14718, 14722, 14726, 14730, 14734, 14738, 14742, 14746, 14750, 14754, 14758, 14762,
+            14766, 14770, 14774, 14778, 14782, 14786, 14794, 14802,
+        };
+
+        private static int[] s_patternUla48_Early_NOP = new int[]
+        {
+            14300, 14304, 14308, 14312, 14316, 14320, 14324, 14328, 14332,
+            14336, 14345, 14353, 14361, 14369, 14377, 14385, 14393, 14401, 14409, 14417, 14425, 14433, 14441, 14449, 14457, 14465, 14469, 14473, 14477, 14481, 14485, 14489, 14493, 14497,
+            14501, 14505, 14509, 14513, 14517, 14521, 14525, 14529, 14533, 14537, 14541, 14545, 14549, 14553, 14557, 14561, 14569, 14577, 14585, 14593, 14601, 14609, 14617, 14625, 14633,
+            14641, 14649, 14657, 14665, 14673, 14681, 14689, 14693, 14697, 14701, 14705, 14709, 14713, 14717, 14721, 14725, 14729, 14733, 
+        };
+
+        private static int[] s_patternUla128_NOP = new int[]
+        {
 			14362, 14372, 14380, 14388, 14396, 14404, 14412, 14420, 14428, 14436, 14444, 14452, 14460, 14468, 14476, 14484, 14492, 
 			14496, 14500, 14504, 14508, 14512, 14516, 14520, 14524, 14528, 14532, 14536, 14540, 14544, 14548, 14552, 14556, 14560, 14564, 14568, 14572, 14576, 14580, 14584, 14588, 14592,
 			14600, 14608, 14616, 14624, 14632, 14640, 14648, 14656, 14664, 14672, 14680, 14688, 14696, 14704, 14712, 14720, 14724, 
 			14728, 14732, 14736,
-		};
+        };
 
-		private static int[] s_patternUla128_INCHL = new int[]
-		{
+        private static int[] s_patternUla128_INCHL = new int[]
+        {
 			14362, 14378, 14394, 14410, 14426, 14442, 14458, 14474, 14490, 
 			14496, 14502, 14508, 14514, 14520, 14526, 14532, 14538, 14544, 14550, 14556, 14562, 14568, 14574, 14580, 14586, 14598,
 			14614, 14630, 14646, 14662, 14678, 14694, 14710, 14722, 14728,
 			14734, 14740, 14746,
-		};
+        };
 
-		private static int[] s_patternUla128_LDA_HL_ = new int[]
-		{
+        private static int[] s_patternUla128_LDA_HL_ = new int[]
+        {
 			14362, 14379, 14395, 14411, 14427, 14443, 14459, 14475, 14491, 
 			14498, 14505, 14512, 14519, 14526, 14533, 14540, 14547, 14554, 14561, 14568, 14575, 14582, 14589, 14599,
 			14615, 14631, 14647, 14663, 14679, 14695, 14711, 14723, 14730, 
 			14737,
-		};
+        };
 
-		private static int[] s_patternUla128_OUTCA = new int[]
-		{
+        private static int[] s_patternUla128_OUTCA = new int[]
+        {
 			14362, 14388, 14402, 14434, 14450, 14476, 14490, 
 			14502, 14508, 14520, 14526, 14538, 14544, 14556, 14562, 14574, 14580, 14592, 
 			14606, 14638, 14654, 14680, 14694, 14720, 
 			14726, 14738, 14744, 14756, 14762, 14774, 14780, 14792, 14798, 14810, 14816,
 			14842, 14858, 14884, 14898, 14930, 14946, 14958, 14964, 14976, 14982,
-		};
+        };
 
-		#endregion
+        #endregion
 
-		private static void runZexall()
+        private static void runZexall()
         {
             SpectrumConcrete p128 = new SpectrumConcrete();
             p128.Init();
@@ -165,9 +174,9 @@ namespace Test
             p128.DoReset();
             p128.ExecuteFrame();
             p128.IsRunning = false;
-			foreach (IKeyboardDevice kbd in p128.BusManager.FindDevices(typeof(IKeyboardDevice)))
-				kbd.KeyboardState = new FakeKeyboardState(Key.Y);
-			
+            foreach (IKeyboardDevice kbd in p128.BusManager.FindDevices(typeof(IKeyboardDevice)))
+                kbd.KeyboardState = new FakeKeyboardState(Key.Y);
+
             using (Stream testStream = GetTestStream("zexall.sna"))
                 p128.Loader.GetSerializer(Path.GetExtension("zexall.sna")).Deserialize(testStream);
             p128.IsRunning = true;
@@ -175,7 +184,7 @@ namespace Test
             for (frame = 0; frame < 700000; frame++)
             {
                 p128.ExecuteFrame();
-                if (frame % 30000 == 0 || ((frame>630000 && frame <660000) && frame%10000==0))
+                if (frame % 30000 == 0 || ((frame > 630000 && frame < 660000) && frame % 10000 == 0))
                 {
                     Console.WriteLine(string.Format("{0:D8}", frame));
                     p128.Loader.SaveFileName(string.Format("{0:D8}.PNG", frame));
@@ -184,55 +193,55 @@ namespace Test
             p128.Loader.SaveFileName(string.Format("{0:D8}.PNG", frame));
         }
 
-		private static void ExecTests(string testName, int frameCount)
-		{
-			SpectrumConcrete p128 = new SpectrumConcrete();
-			p128.Init();
-			p128.IsRunning = true;
-			p128.DoReset();
-			p128.ExecuteFrame();
+        private static void ExecTests(string testName, int frameCount)
+        {
+            SpectrumConcrete p128 = new SpectrumConcrete();
+            p128.Init();
+            p128.IsRunning = true;
+            p128.DoReset();
+            p128.ExecuteFrame();
 
-			p128.IsRunning = false;
-			using (Stream testStream = GetTestStream(testName))
-				p128.Loader.GetSerializer(Path.GetExtension(testName)).Deserialize(testStream);
-			p128.IsRunning = true;
+            p128.IsRunning = false;
+            using (Stream testStream = GetTestStream(testName))
+                p128.Loader.GetSerializer(Path.GetExtension(testName)).Deserialize(testStream);
+            p128.IsRunning = true;
 
 
-			Stopwatch watch = new Stopwatch();
-			watch.Start();
-			for (int frame = 0; frame < frameCount; frame++)
-				p128.ExecuteFrame();
-			watch.Stop();
-			Console.WriteLine("{0}:\t{1} [ms]", testName, watch.ElapsedMilliseconds);
-			//p128.Loader.SaveFileName(testName);
-		}
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int frame = 0; frame < frameCount; frame++)
+                p128.ExecuteFrame();
+            watch.Stop();
+            Console.WriteLine("{0}:\t{1} [ms]", testName, watch.ElapsedMilliseconds);
+            //p128.Loader.SaveFileName(testName);
+        }
 
-		private static Stream GetTestStream(string testName)
-		{
-			testName = string.Format("Test.{0}", testName);
-			return Assembly.GetExecutingAssembly().GetManifestResourceStream(testName);
-		}
-	}
+        private static Stream GetTestStream(string testName)
+        {
+            testName = string.Format("Test.{0}", testName);
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream(testName);
+        }
+    }
 
-	public class FakeKeyboardState : IKeyboardState
-	{
-		private bool m_pressSimulation = false;
-		private Key m_key;
+    public class FakeKeyboardState : IKeyboardState
+    {
+        private bool m_pressSimulation = false;
+        private Key m_key;
 
-		public FakeKeyboardState()
-		{
-			m_pressSimulation = false;
-		}
-		
-		public FakeKeyboardState(Key keyPressed)
-		{
-			m_key = keyPressed;
-			m_pressSimulation = true;
-		}
+        public FakeKeyboardState()
+        {
+            m_pressSimulation = false;
+        }
 
-		public bool this[Key key]
-		{
-			get { return m_pressSimulation ? (key == m_key) : false; }
-		}
-	}
+        public FakeKeyboardState(Key keyPressed)
+        {
+            m_key = keyPressed;
+            m_pressSimulation = true;
+        }
+
+        public bool this[Key key]
+        {
+            get { return m_pressSimulation ? (key == m_key) : false; }
+        }
+    }
 }
--- ZXMAK2/Engine/Devices/Ula/UlaSpectrum48.cs
+++ ZXMAK2/Engine/Devices/Ula/UlaSpectrum48.cs
@@ -6,179 +6,192 @@ using ZXMAK2.Engine.Interfaces;
 
 namespace ZXMAK2.Engine.Devices.Ula
 {
-	public class UlaSpectrum48 : UlaDeviceBase
-	{
-		#region IBusDevice
+    public class UlaSpectrum48 : UlaDeviceBase
+    {
+        #region IBusDevice
 
-		public override string Name { get { return "ZX Spectrum 48"; } }
+        public override string Name { get { return "ZX Spectrum 48"; } }
 
-		public override void BusInit(IBusManager bmgr)
-		{
-			base.BusInit(bmgr);
-			bmgr.SubscribeRDMEM(0xC000, 0x4000, ReadMem4000);
-			bmgr.SubscribeRDMEM_M1(0xC000, 0x4000, ReadMem4000);
+        public override void BusInit(IBusManager bmgr)
+        {
+            base.BusInit(bmgr);
+            bmgr.SubscribeRDMEM(0xC000, 0x4000, ReadMem4000);
+            bmgr.SubscribeRDMEM_M1(0xC000, 0x4000, ReadMem4000);
 
-			bmgr.SubscribeRDNOMREQ(0xC000, 0x4000, ContendNoMreq);
-			bmgr.SubscribeWRNOMREQ(0xC000, 0x4000, ContendNoMreq);
+            bmgr.SubscribeRDNOMREQ(0xC000, 0x4000, ContendNoMreq);
+            bmgr.SubscribeWRNOMREQ(0xC000, 0x4000, ContendNoMreq);
 
-			bmgr.SubscribeRDIO(0x0000, 0x0000, ReadPortAll);
-			bmgr.SubscribeWRIO(0x0000, 0x0000, WritePortAll);
+            if (m_lateModel)
+                bmgr.SubscribeRDIO(0x0000, 0x0000, ReadPortAllLate);
+            else
+                bmgr.SubscribeRDIO(0x0000, 0x0000, ReadPortAllEarly);
+            bmgr.SubscribeWRIO(0x0000, 0x0000, WritePortAll);
         }
 
-		#endregion
+        #endregion
 
-		public UlaSpectrum48()
-		{
-			// ZX Spectrum 48
-			// Total Size:          //+ 448 x 312
-			// Visible Size:        //+ 352 x 303 (48+256+48 x 55+192+56)
+        public UlaSpectrum48()
+        {
+            // ZX Spectrum 48
+            // Total Size:          //+ 448 x 312
+            // Visible Size:        //+ 352 x 303 (48+256+48 x 55+192+56)
 
-			c_ulaLineTime = 224;
-			c_ulaFirstPaperLine = 64;
-			c_ulaFirstPaperTact = 64;      // 64 [40sync+24border+128scr+32border]
-			c_frameTactCount = 69888;
+            c_ulaLineTime = 224;
+            c_ulaFirstPaperLine = 64;
+            c_ulaFirstPaperTact = 64;      // 64 [40sync+24border+128scr+32border]
+            c_frameTactCount = 69888;
             c_ulaBorder4T = true;
             c_ulaBorder4Tstage = 1;
 
-			c_ulaBorderTop = 55;      //56 (at least 48=border, other=retrace or border)
-			c_ulaBorderBottom = 56;   //
-			c_ulaBorderLeftT = 24;    //16T
-			c_ulaBorderRightT = 24;   //32T
+            c_ulaBorderTop = 55;      //56 (at least 48=border, other=retrace or border)
+            c_ulaBorderBottom = 56;   //
+            c_ulaBorderLeftT = 24;    //16T
+            c_ulaBorderRightT = 24;   //32T
 
             c_ulaIntBegin = 62 + 1;
             c_ulaIntLength = 32;    // according to fuse
 
-			c_ulaWidth = (c_ulaBorderLeftT + 128 + c_ulaBorderRightT) * 2;
-			c_ulaHeight = (c_ulaBorderTop + 192 + c_ulaBorderBottom);
-			
+            c_ulaWidth = (c_ulaBorderLeftT + 128 + c_ulaBorderRightT) * 2;
+            c_ulaHeight = (c_ulaBorderTop + 192 + c_ulaBorderBottom);
+
             fillTable(true);
-		}
-
-
-		#region Bus Handlers
-
-		protected override void WriteMem4000(ushort addr, byte value)
-		{
-			contendMemory();
-			base.WriteMem4000(addr, value);
-		}
-
-		protected void ReadMem4000(ushort addr, ref byte value)
-		{
-			contendMemory();
-		}
-
-		protected void ContendNoMreq(ushort addr)
-		{
-			if (IsContended(addr))
-				contendMemory();
         }
 
-		protected override void WritePortFE(ushort addr, byte value, ref bool iorqge)
-		{
-		}
 
-		private void WritePortAll(ushort addr, byte value, ref bool iorqge)
-		{
+        #region Bus Handlers
+
+        protected override void WriteMem4000(ushort addr, byte value)
+        {
+            contendMemory();
+            base.WriteMem4000(addr, value);
+        }
+
+        protected void ReadMem4000(ushort addr, ref byte value)
+        {
+            contendMemory();
+        }
+
+        protected void ContendNoMreq(ushort addr)
+        {
+            if (IsContended(addr))
+                contendMemory();
+        }
+
+        protected override void WritePortFE(ushort addr, byte value, ref bool iorqge)
+        {
+        }
+
+        private void WritePortAll(ushort addr, byte value, ref bool iorqge)
+        {
             contendPortEarly(addr);
             contendPortLate(addr);
             if ((addr & 0x0001) == 0)
-			{
-				int frameTact = (int)((CPU.Tact - 1) % FrameTactCount);
+            {
+                int frameTact = (int)((CPU.Tact - 1) % FrameTactCount);
                 UpdateState(frameTact);
-				PortFE = value;
-			}
+                PortFE = value;
+            }
         }
 
-		private void ReadPortAll(ushort addr, ref byte value, ref bool iorqge)
-		{
-			contendPortEarly(addr);
+        private void ReadPortAllEarly(ushort addr, ref byte value, ref bool iorqge)
+        {
+            contendPortEarly(addr);
+            contendPortLate(addr);
+            int frameTact = (int)((CPU.Tact) % FrameTactCount);
+            base.ReadPortFF(frameTact, ref value);
+        }
+
+        private void ReadPortAllLate(ushort addr, ref byte value, ref bool iorqge)
+        {
+            contendPortEarly(addr);
             contendPortLate(addr);
             int frameTact = (int)((CPU.Tact - 1) % FrameTactCount);
             base.ReadPortFF(frameTact, ref value);
         }
 
 
-		#endregion
+        #endregion
 
-		private void contendMemory()
-		{
-			int frameTact = (int)(CPU.Tact % c_frameTactCount);
-			CPU.Tact += m_contention[frameTact];
-		}
+        private void contendMemory()
+        {
+            int frameTact = (int)(CPU.Tact % c_frameTactCount);
+            CPU.Tact += m_contention[frameTact];
+        }
 
-		private bool IsContended(int addr)
-		{
-			int test = addr & 0xC000;
-			return (test == 0x4000);
-		}
+        private bool IsContended(int addr)
+        {
+            int test = addr & 0xC000;
+            return (test == 0x4000);
+        }
 
-		private bool IsPortUla(int addr)
-		{
-			return (addr & 1) == 0;
-		}
+        private bool IsPortUla(int addr)
+        {
+            return (addr & 1) == 0;
+        }
 
-		private void contendPortEarly(int addr)
-		{
-			if (IsContended(addr))
-			{
-				int frameTact = (int)(CPU.Tact % c_frameTactCount);
-				CPU.Tact += m_contention[frameTact];
-			}
-		}
+        private void contendPortEarly(int addr)
+        {
+            if (IsContended(addr))
+            {
+                int frameTact = (int)(CPU.Tact % c_frameTactCount);
+                CPU.Tact += m_contention[frameTact];
+            }
+        }
 
-		private void contendPortLate(int addr)
-		{
-			int shift = 1;
-			int frameTact = (int)((CPU.Tact + shift) % c_frameTactCount);
+        private void contendPortLate(int addr)
+        {
+            int shift = 1;
+            int frameTact = (int)((CPU.Tact + shift) % c_frameTactCount);
 
-			if (IsPortUla(addr))
-			{
-				CPU.Tact += m_contention[frameTact];
-			}
-			else if (IsContended(addr))
-			{
-				CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
-				CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
-				CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
-			}
-		}
+            if (IsPortUla(addr))
+            {
+                CPU.Tact += m_contention[frameTact];
+            }
+            else if (IsContended(addr))
+            {
+                CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
+                CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
+                CPU.Tact += m_contention[frameTact]; frameTact += m_contention[frameTact]; frameTact++; frameTact %= c_frameTactCount;
+            }
+        }
 
 
-		protected void fillTable(bool lateModel)
-		{
-			m_contention = new int[c_frameTactCount];
-			int[] byteContention = new int[] { 6, 5, 4, 3, 2, 1, 0, 0, };
-			for (int t = 0; t < c_frameTactCount; t++)
-			{
+        protected void fillTable(bool lateModel)
+        {
+            m_lateModel = lateModel;
+            m_contention = new int[c_frameTactCount];
+            int[] byteContention = new int[] { 6, 5, 4, 3, 2, 1, 0, 0, };
+            for (int t = 0; t < c_frameTactCount; t++)
+            {
                 int shifted = t - c_ulaIntBegin;
                 if (!lateModel)
-                    shifted-=1;
+                    shifted -= 1;
                 if (shifted < 0)
                     shifted += c_frameTactCount;
 
                 m_contention[shifted] = 0;
-				int line = t / c_ulaLineTime;
-				int pix = t % c_ulaLineTime;
-				if (line < c_ulaFirstPaperLine || line >= (c_ulaFirstPaperLine + 192))
-				{
-					m_contention[shifted] = 0;
-					continue;
-				}
-				int scrPix = pix - c_ulaFirstPaperTact + 1;
-				if (scrPix < 0 || scrPix >= 128)
-				{
-					m_contention[shifted] = 0;
-					continue;
-				}
-				int pixByte = scrPix % 8;
+                int line = t / c_ulaLineTime;
+                int pix = t % c_ulaLineTime;
+                if (line < c_ulaFirstPaperLine || line >= (c_ulaFirstPaperLine + 192))
+                {
+                    m_contention[shifted] = 0;
+                    continue;
+                }
+                int scrPix = pix - c_ulaFirstPaperTact + 1;
+                if (scrPix < 0 || scrPix >= 128)
+                {
+                    m_contention[shifted] = 0;
+                    continue;
+                }
+                int pixByte = scrPix % 8;
 
-				m_contention[shifted] = byteContention[pixByte];
-			}
-		}
+                m_contention[shifted] = byteContention[pixByte];
+            }
+        }
 
-		private int[] m_contention;
-	}
+        private bool m_lateModel;
+        private int[] m_contention;
+    }
 
     public class UlaSpectrum48_Early : UlaSpectrum48
     {
