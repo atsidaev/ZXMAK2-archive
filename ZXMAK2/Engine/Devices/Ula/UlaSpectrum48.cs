b064ebed58a5705e7a5282b262dcf16fcc0250e5
Alex Makeev <zxmak@mail.ru>
2012-05-26 14:50:11 +0000

    fix ULA 48 early/late

--- ZXMAK2/Engine/Devices/Ula/UlaSpectrum48.cs
+++ ZXMAK2/Engine/Devices/Ula/UlaSpectrum48.cs
@@ -79,13 +79,13 @@ namespace ZXMAK2.Engine.Devices.Ula
         private void WritePortAll(ushort addr, byte value, ref bool iorqge)
         {
             contendPortEarly(addr);
-            contendPortLate(addr);
             if ((addr & 0x0001) == 0)
             {
                 int frameTact = (int)((CPU.Tact-2) % FrameTactCount);
                 UpdateState(frameTact);
                 PortFE = value;
             }
+            contendPortLate(addr);
         }
 
         private void ReadPortAll(ushort addr, ref byte value, ref bool iorqge)
