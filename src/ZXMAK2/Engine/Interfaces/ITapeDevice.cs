a390d5c1321e6359f66dd87a6e38a6a05800f933
Alex Makeev <zxmak@mail.ru>
2012-06-02 12:16:42 +0000

    add tape AutoPlay feature (thanks to Woody for algorithm)

--- src/ZXMAK2/Engine/Interfaces/ITapeDevice.cs
+++ src/ZXMAK2/Engine/Interfaces/ITapeDevice.cs
@@ -4,37 +4,38 @@ using System.Collections.Generic;
 
 namespace ZXMAK2.Engine.Interfaces
 {
-    public interface ITapeDevice //: BusDeviceBase
-    {
-        void Play();
-        void Stop();
-        void Rewind();
-        void Reset();       // loaded new image
-        bool IsPlay { get; }
-        int TactsPerSecond { get; }
-        List<TapeBlock> Blocks { get; }
-        event EventHandler TapeStateChanged;
-        int CurrentBlock { get; set; }
-        int Position { get; }
-        bool TrapsAllowed { get; set; }
-    }
+	public interface ITapeDevice
+	{
+		void Play();
+		void Stop();
+		void Rewind();
+		void Reset();       // loaded new image
+		bool IsPlay { get; }
+		int TactsPerSecond { get; }
+		List<TapeBlock> Blocks { get; }
+		event EventHandler TapeStateChanged;
+		int CurrentBlock { get; set; }
+		int Position { get; }
+		bool UseTraps { get; set; }
+		bool UseAutoPlay { get; set; }
+	}
 
-    public enum TapeCommand
-    {
-        NONE,
-        STOP_THE_TAPE,
-        STOP_THE_TAPE_48K,
-        BEGIN_GROUP,
-        END_GROUP,
-        SHOW_MESSAGE,
-    }
+	public enum TapeCommand
+	{
+		NONE,
+		STOP_THE_TAPE,
+		STOP_THE_TAPE_48K,
+		BEGIN_GROUP,
+		END_GROUP,
+		SHOW_MESSAGE,
+	}
 
-    public class TapeBlock
-    {
-        public string Description;
-        public int TzxId = -1;
-        public List<int> Periods = new List<int>();
+	public class TapeBlock
+	{
+		public string Description;
+		public int TzxId = -1;
+		public List<int> Periods = new List<int>();
 		public byte[] TapData = null;
 		public TapeCommand Command = TapeCommand.NONE;
-    }
+	}
 }
