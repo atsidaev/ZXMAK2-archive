42af5b9a074e307f332bc8c4b0798ac12beb8cda
Alex Makeev <zxmak@mail.ru>
2012-06-04 21:42:14 +0000

    fix port #FE emulation for Scorpion ULA/Beeper

--- ZXMAK2/Engine/Devices/BeeperScorpion.cs
+++ ZXMAK2/Engine/Devices/BeeperScorpion.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ZXMAK2.Engine.Interfaces;
 
 namespace ZXMAK2.Engine.Devices
 {
-    public class BeeperScorpion : BeeperDevice
-    {
-        #region IBusDevice
+	public class BeeperScorpion : BeeperDevice
+	{
+		#region IBusDevice
 
-        public override string Name { get { return "Scorpion Beeper"; } }
-        public override string Description { get { return "Simple Scorpion ZS Beeper"; } }
+		public override string Name { get { return "Scorpion Beeper"; } }
+		public override string Description { get { return "Simple Scorpion ZS Beeper"; } }
 
-        #endregion
+		public override void BusInit(IBusManager bmgr)
+		{
+			base.BusInit(bmgr);
+			m_memory = (IMemoryDevice)bmgr.FindDevice(typeof(IMemoryDevice));
+		}
 
-        #region BeeperDevice
+		#endregion
 
-        protected override void WritePortFE(ushort addr, byte value, ref bool iorqge)
-        {
-            if ((addr & 0x23) == (0xFE & 0x23))  // TODO: check no trdos port activated
-                base.WritePortFE(addr, value, ref iorqge);
-        }
+		#region BeeperDevice
 
-        #endregion
-    }
+		protected override void WritePortFE(ushort addr, byte value, ref bool iorqge)
+		{
+			if (!m_memory.DOSEN && (addr & 0x23) == (0xFE & 0x23))
+				base.WritePortFE(addr, value, ref iorqge);
+		}
+
+		#endregion
+
+		private IMemoryDevice m_memory;
+	}
 }
