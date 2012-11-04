ca5ed38e2115c562aa3c3e4ca1f54cdd141a8497
Alex Makeev <zxmak@mail.ru>
2012-11-04 19:07:03 +0000

    add profi memory with old rom image

--- src/ZXMAK2/Hardware/Profi/MemoryProfi1024.cs
+++ src/ZXMAK2/Hardware/Profi/MemoryProfi1024.cs
@@ -7,129 +7,140 @@ using ZXMAK2.Engine.Z80;
 
 namespace ZXMAK2.Hardware.Profi
 {
-    public class MemoryProfi1024 : MemoryBase
-    {
-        #region IBusDevice
+	public class MemoryProfi1024 : MemoryBase
+	{
+		#region IBusDevice
 
-        public override string Name { get { return "PROFI+ 1024K"; } }
-        public override string Description { get { return "PROFI+ 1024K Memory Manager"; } }
+		public override string Name { get { return "PROFI+ 1024K"; } }
+		public override string Description { get { return "PROFI+ 1024K Memory Manager"; } }
 
-        public override void BusInit(IBusManager bmgr)
-        {
-            base.BusInit(bmgr);
-            m_cpu = bmgr.CPU;
-            m_ulaProfi = bmgr.FindDevice(typeof(UlaProfi3XX)) as UlaProfi3XX;
+		public override void BusInit(IBusManager bmgr)
+		{
+			base.BusInit(bmgr);
+			m_cpu = bmgr.CPU;
+			m_ulaProfi = bmgr.FindDevice(typeof(UlaProfi3XX)) as UlaProfi3XX;
 
 			bmgr.SubscribeWRIO(0x8002, 0x0000, writePort7FFD);
-            bmgr.SubscribeWRIO(0xFFFF, 0xDFFD, writePortDFFD);
-            bmgr.SubscribeRDMEM_M1(0xC000, 0x4000, readRamM1);
-            bmgr.SubscribeRDMEM_M1(0xC000, 0x8000, readRamM1);
-            bmgr.SubscribeRDMEM_M1(0xC000, 0xC000, readRamM1);
+			bmgr.SubscribeWRIO(0xFFFF, 0xDFFD, writePortDFFD);
+			bmgr.SubscribeRDMEM_M1(0xC000, 0x4000, readRamM1);
+			bmgr.SubscribeRDMEM_M1(0xC000, 0x8000, readRamM1);
+			bmgr.SubscribeRDMEM_M1(0xC000, 0xC000, readRamM1);
 
-            bmgr.SubscribeRESET(busReset);
-        }
+			bmgr.SubscribeRESET(busReset);
+		}
 
-        #endregion
+		#endregion
 
-        #region MemoryBase
+		#region MemoryBase
 
-        public override byte[][] RamPages { get { return m_ramPages; } }
+		public override byte[][] RamPages { get { return m_ramPages; } }
 
 		public override bool IsMap48 { get { return false; } }
 
 		protected override void UpdateMapping()
-        {
-            m_lock = ((CMR0 & 0x20) != 0);
-            int ramPage = CMR0 & 7;
-            int romPage = (CMR0 & 0x10) >> 4;
+		{
+			m_lock = ((CMR0 & 0x20) != 0);
+			int ramPage = CMR0 & 7;
+			int romPage = (CMR0 & 0x10) >> 4;
 			int videoPage = (CMR0 & 0x08) == 0 ? 5 : 7;
 
-            if (DOSEN)      // trdos or 48/128
-                romPage = 2;
-            if (SYSEN)
-                romPage = 3;
+			if (DOSEN)      // trdos or 48/128
+				romPage = 2;
+			if (SYSEN)
+				romPage = 3;
 
-            int sega = CMR1 & 7;
-            bool norom = (CMR1 & 0x10) != 0;
-            bool sco = (CMR1 & 0x08) != 0;   // selectors RAM gates
-            bool scr = (CMR1 & 0x40) != 0;   // !??CMR0.D3=1??!
-            //bool cpm = (CMR1 & 0x20) != 0;
-            bool ds80 = (CMR1 & 0x80) != 0;
+			int sega = CMR1 & 7;
+			bool norom = (CMR1 & 0x10) != 0;
+			bool sco = (CMR1 & 0x08) != 0;   // selectors RAM gates
+			bool scr = (CMR1 & 0x40) != 0;   // !??CMR0.D3=1??!
+			//bool cpm = (CMR1 & 0x20) != 0;
+			bool ds80 = (CMR1 & 0x80) != 0;
 
-            if (norom)
-                m_lock = false;
-            
-            ramPage |= sega << 3;
+			if (norom)
+				m_lock = false;
 
-			if(m_ulaProfi!=null)
-				m_ulaProfi.SetPageMappingProfi(videoPage, norom? 0:-1, sco ? ramPage : 5, scr? 6:2, sco ? 7 : ramPage, ds80);
+			ramPage |= sega << 3;
+
+			if (m_ulaProfi != null)
+				m_ulaProfi.SetPageMappingProfi(videoPage, norom ? 0 : -1, sco ? ramPage : 5, scr ? 6 : 2, sco ? 7 : ramPage, ds80);
 			else
-				m_ula.SetPageMapping(videoPage, norom? 0:-1, sco ? ramPage : 5, scr? 6:2, sco ? 7 : ramPage);
+				m_ula.SetPageMapping(videoPage, norom ? 0 : -1, sco ? ramPage : 5, scr ? 6 : 2, sco ? 7 : ramPage);
 			MapRead0000 = norom ? RamPages[0] : RomPages[romPage];
-            MapRead4000 = sco ? RamPages[ramPage] : RamPages[5];
-            MapRead8000 = scr ? RamPages[6] : RamPages[2];
-            MapReadC000 = sco ? RamPages[7] : RamPages[ramPage];
-            
-            MapWrite0000 = norom ? RamPages[0] : m_trashPage;
-            MapWrite4000 = MapRead4000;
-            MapWrite8000 = MapRead8000;
-            MapWriteC000 = MapReadC000;
-        }
+			MapRead4000 = sco ? RamPages[ramPage] : RamPages[5];
+			MapRead8000 = scr ? RamPages[6] : RamPages[2];
+			MapReadC000 = sco ? RamPages[7] : RamPages[ramPage];
 
-        protected override void LoadRom()
-        {
-            base.LoadRom();
+			MapWrite0000 = norom ? RamPages[0] : m_trashPage;
+			MapWrite4000 = MapRead4000;
+			MapWrite8000 = MapRead8000;
+			MapWriteC000 = MapReadC000;
+		}
+
+		protected override void LoadRom()
+		{
+			base.LoadRom();
 			LoadRomPack("Profi");
 		}
 
-        #endregion
+		#endregion
 
-        #region Bus Handlers
+		#region Bus Handlers
 
-        private void writePort7FFD(ushort addr, byte value, ref bool iorqge)
-        {
-            if (!iorqge)
-                return;
-            iorqge = false;
-            if (!m_lock)
-                CMR0 = value;
-        }
+		private void writePort7FFD(ushort addr, byte value, ref bool iorqge)
+		{
+			if (!iorqge)
+				return;
+			iorqge = false;
+			if (!m_lock)
+				CMR0 = value;
+		}
 
-        private void writePortDFFD(ushort addr, byte value, ref bool iorqge)
-        {
-            if (!iorqge)
-                return;
-            iorqge = false;
-            CMR1 = value;
-        }
+		private void writePortDFFD(ushort addr, byte value, ref bool iorqge)
+		{
+			if (!iorqge)
+				return;
+			iorqge = false;
+			CMR1 = value;
+		}
 
-        private void readRamM1(ushort addr, ref byte value)
-        {
-            if (SYSEN)
-                SYSEN = false;
-        }
+		private void readRamM1(ushort addr, ref byte value)
+		{
+			if (SYSEN)
+				SYSEN = false;
+		}
 
-        private void busReset()
-        {
-            CMR1 = 0;
-            CMR0 = 0;
-            SYSEN = true;
-        }
+		private void busReset()
+		{
+			CMR1 = 0;
+			CMR0 = 0;
+			SYSEN = true;
+		}
 
-        #endregion
-
-
-        private byte[][] m_ramPages = new byte[64][];
-        private byte[] m_trashPage = new byte[0x4000];
-        private Z80CPU m_cpu;
-        private UlaProfi3XX m_ulaProfi;
-        private bool m_lock = false;
+		#endregion
 
 
-        public MemoryProfi1024()
-        {
-            for (int i = 0; i < m_ramPages.Length; i++)
-                m_ramPages[i] = new byte[0x4000];
-        }
-    }
+		private byte[][] m_ramPages = new byte[64][];
+		private byte[] m_trashPage = new byte[0x4000];
+		private Z80CPU m_cpu;
+		private UlaProfi3XX m_ulaProfi;
+		private bool m_lock = false;
+
+
+		public MemoryProfi1024()
+		{
+			for (int i = 0; i < m_ramPages.Length; i++)
+				m_ramPages[i] = new byte[0x4000];
+		}
+	}
+
+	public class MemoryProfi3XX : MemoryProfi1024
+	{
+		public override string Name { get { return "PROFI+ [3.xx]"; } }
+
+		protected override void LoadRom()
+		{
+			base.LoadRom();
+			LoadRomPack("Profi3XX");
+		}
+	}
 }
