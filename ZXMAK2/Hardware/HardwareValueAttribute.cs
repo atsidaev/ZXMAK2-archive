e4123c848726d8a832164ef6aef6b43aa78d9feb
Alex Makeev <zxmak@mail.ru>
2012-11-11 02:42:50 +0000

    add ATM710 memory module (trdos mapping not implemented yet)

---
+++ ZXMAK2/Hardware/HardwareValueAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZXMAK2.Hardware
+{
+	[System.AttributeUsage(System.AttributeTargets.Property)]
+	public class HardwareValueAttribute : System.Attribute
+	{
+		private string m_name;
+		public string Description;
+
+
+		public HardwareValueAttribute(string name)
+		{
+			m_name = name;
+		}
+	}
+}
