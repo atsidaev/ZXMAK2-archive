e68b26af70bbe9d2a44a5f0eb93a5b4634435377
Alex Makeev <zxmak@mail.ru>
2013-01-20 09:13:39 +0000

    move new debugger to Hardware/Adlers & rollback general debugger changes

--- ZXMAK2/Hardware/Adlers/UI/DictionarySafe.cs
+++ ZXMAK2/Hardware/Adlers/UI/DictionarySafe.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ZXMAK2.Controls.Debugger
+namespace ZXMAK2.Hardware.Adlers.UI
 {
     public class DictionarySafe<TKey, TValue> : IDictionary<TKey, TValue>
     {
