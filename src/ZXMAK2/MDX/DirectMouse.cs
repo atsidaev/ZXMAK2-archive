0da07649d57dd51c0f31f9d6a5c86694019e4c53
Alex Makeev <zxmak@mail.ru>
2013-05-04 13:39:17 +0000

    #1380 - Impossible to use Alt key without menu activation

--- src/ZXMAK2/MDX/DirectMouse.cs
+++ src/ZXMAK2/MDX/DirectMouse.cs
@@ -8,119 +8,137 @@ using Microsoft.DirectX.DirectInput;
 
 namespace ZXMAK2.MDX
 {
-	public class DirectMouse : IDisposable
-	{
-		private Form _form;
-		private bool _active = false;
-		private Device _device = null;
+    public class DirectMouse : IDisposable
+    {
+        private Form m_form;
+        private bool m_active = false;
+        private Device m_device = null;
+        private MouseStateWrapper m_state = new MouseStateWrapper();
+
+        
+        public ZXMAK2.Interfaces.IMouseState MouseState
+        {
+            get { return m_state; }
+        }
+
+        public bool IsCaptured
+        {
+            get { return m_active; }
+        }
 
 
-		public DirectMouse(Form mainForm)
-		{
-			_form = mainForm;
-			if (_device == null)
-			{
-				_device = new Device(SystemGuid.Mouse);
-				//            mainForm.Activated += WndActivated;
-				mainForm.Deactivate += WndDeactivate;
-				//            WndActivated(null, null);
-			}
-		}
-		public void Dispose()
-		{
-			if (_device != null)
-			{
-				_active = false;
-				_device.Unacquire();
-				_device.Dispose();
-				_device = null;
-			}
-		}
+        public DirectMouse(Form mainForm)
+        {
+            m_form = mainForm;
+            if (m_device == null)
+            {
+                m_device = new Device(SystemGuid.Mouse);
+                mainForm.Deactivate += WndDeactivate;
+            }
+        }
 
-		private void WndActivated(object sender, EventArgs e)
-		{
-		}
-		private void WndDeactivate(object sender, EventArgs e)
-		{
-			StopCapture();
-		}
+        public void Dispose()
+        {
+            if (m_device != null)
+            {
+                m_active = false;
+                m_device.Unacquire();
+                m_device.Dispose();
+                m_device = null;
+            }
+        }
 
-		public void StartCapture()
-		{
-			if (_device != null && !_active)
-			{
-				try
-				{
-					_device.SetCooperativeLevel(
-					   _form,
-					   CooperativeLevelFlags.Exclusive | CooperativeLevelFlags.Foreground
-					   );
-					_device.Acquire();
-					_active = true;
-				}
-				catch { StopCapture(); }
-			}
-		}
-		public void StopCapture()
-		{
-			if (_device != null)
-			{
-				try
-				{
-					if (_active)
-						_device.Unacquire();
-					_device.SetCooperativeLevel(_form,
-					   CooperativeLevelFlags.NonExclusive | CooperativeLevelFlags.Foreground
-					   );
-					_active = false;
-				}
-				catch { }
-			}
-		}
+        private void WndDeactivate(object sender, EventArgs e)
+        {
+            StopCapture();
+        }
 
-		private MouseStateWrapper m_state = new MouseStateWrapper();
-		public ZXMAK2.Interfaces.IMouseState MouseState { get { return m_state; } }
+        public void StartCapture()
+        {
+            if (m_device != null && !m_active)
+            {
+                try
+                {
+                    m_device.SetCooperativeLevel(
+                        m_form,
+                        CooperativeLevelFlags.Exclusive |
+                            CooperativeLevelFlags.Foreground);
+                    m_device.Acquire();
+                    m_active = true;
+                }
+                catch
+                {
+                    StopCapture();
+                }
+            }
+        }
 
-		public void Scan()
-		{
-			if (_active)
-			{
-				try
-				{
-					m_state.Update(_device.CurrentMouseState);
-				}
-				catch (NotAcquiredException) { StopCapture(); return; }
-			}
-		}
-		
-		private class MouseStateWrapper : ZXMAK2.Interfaces.IMouseState
-		{
-			private int m_x = 0;
-			private int m_y = 0;
-			private int m_b = 0;
+        public void StopCapture()
+        {
+            if (m_device != null)
+            {
+                try
+                {
+                    if (m_active)
+                    {
+                        m_device.Unacquire();
+                    }
+                    m_device.SetCooperativeLevel(
+                        m_form,
+                        CooperativeLevelFlags.NonExclusive | 
+                            CooperativeLevelFlags.Foreground);
+                    m_active = false;
+                }
+                catch
+                {
+                }
+            }
+        }
 
-			internal MouseStateWrapper()
-			{
-			}
+        public void Scan()
+        {
+            if (m_active)
+            {
+                try
+                {
+                    m_state.Update(m_device.CurrentMouseState);
+                }
+                catch (NotAcquiredException)
+                {
+                    StopCapture();
+                    return;
+                }
+            }
+        }
 
-			internal void Update(MouseState state)
-			{
-				m_x += state.X;
-				m_y += state.Y;
+        private class MouseStateWrapper : ZXMAK2.Interfaces.IMouseState
+        {
+            private int m_x = 0;
+            private int m_y = 0;
+            private int m_b = 0;
 
-				m_b = 0;
-				byte[] buttonState = state.GetMouseButtons();
-				if ((buttonState[0] & 0x80) != 0) m_b |= 1;
-				if ((buttonState[1] & 0x80) != 0) m_b |= 2;
-				if ((buttonState[2] & 0x80) != 0) m_b |= 4;
-				if ((buttonState[3] & 0x80) != 0) m_b |= 8;
-				if ((buttonState[4] & 0x80) != 0) m_b |= 16;
-				if ((buttonState[5] & 0x80) != 0) m_b |= 32;
-			}
+            internal MouseStateWrapper()
+            {
+            }
 
-			public int X { get { return m_x; } }
-			public int Y { get { return m_y; } }
-			public int Buttons { get { return m_b; } }
-		}
-	}
+            internal void Update(MouseState state)
+            {
+                m_x += state.X;
+                m_y += state.Y;
+
+                m_b = 0;
+                byte[] buttonState = state.GetMouseButtons();
+                if ((buttonState[0] & 0x80) != 0) m_b |= 1;
+                if ((buttonState[1] & 0x80) != 0) m_b |= 2;
+                if ((buttonState[2] & 0x80) != 0) m_b |= 4;
+                if ((buttonState[3] & 0x80) != 0) m_b |= 8;
+                if ((buttonState[4] & 0x80) != 0) m_b |= 16;
+                if ((buttonState[5] & 0x80) != 0) m_b |= 32;
+            }
+
+            public int X { get { return m_x; } }
+            public int Y { get { return m_y; } }
+            public int Buttons { get { return m_b; } }
+        }
+    }
 }
