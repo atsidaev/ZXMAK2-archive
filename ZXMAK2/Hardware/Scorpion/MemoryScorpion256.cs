0eed74515e7a10fb16de4347b44afb4ca8e0733b
Alex Makeev <zxmak@mail.ru>
2013-05-08 05:10:27 +0000

    add Scorpion PROF-ROM 1024K memory

--- ZXMAK2/Hardware/Scorpion/MemoryScorpion256.cs
+++ ZXMAK2/Hardware/Scorpion/MemoryScorpion256.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using ZXMAK2.Interfaces;
 using ZXMAK2.Engine.Z80;
 
@@ -191,87 +190,6 @@ namespace ZXMAK2.Hardware.Scorpion
             sega |= (CMR1 & 0xC0) >> 5;		// 1024 extension
             ramPage |= sega << 3;
             return ramPage;
-        }
-    }
-
-    public class MemoryScorpionProfRom256 : MemoryScorpion256
-    {
-        #region IBusDevice
-
-        public override string Name { get { return "Scorpion PROF-ROM 256K"; } }
-        public override string Description { get { return "Scorpion PROF-ROM 256K Memory Manager"; } }
-
-        public override void BusInit(IBusManager bmgr)
-        {
-            base.BusInit(bmgr);
-            bmgr.SubscribeRdMemM1(0xFFF0, 0x0100, busProfRomGate);
-            bmgr.SubscribeRdMem(0xFFF0, 0x0100, busProfRomGate);
-        }
-
-        #endregion
-
-        #region Bus Handlers
-
-        protected virtual void busProfRomGate(ushort addr, ref byte value)
-        {
-            if (!SYSEN)	// 2, 6, А, Е
-                return;
-            int newPlane = s_profPlaneMap[(addr & 0x0C) | (m_profPlane & 0x03)];
-            if (m_profPlane != newPlane)
-            {
-                m_profPlane = newPlane;
-                UpdateMapping();
-            }
-        }
-
-        private static readonly int[] s_profPlaneMap = new int[]
-		{
-			0, 1, 2, 3,
-			3, 3, 3, 2,
-			2, 2, 0, 1,
-			1, 0, 1, 0,
-		};
-
-        #endregion
-
-
-        protected byte[][] m_romPages = new byte[16][];
-        private int m_profPlane = 0;
-
-
-        public MemoryScorpionProfRom256()
-            : base("Scorpion-ProfRom")
-        {
-        }
-
-        public override byte[][] RomPages { get { return m_romPages; } }
-
-        // needs to allow enable DOS when m_profPlane!=0
-        public override bool IsRom48
-        {
-            get { return !SYSEN && !DOSEN && (CMR0 & 0x10) != 0; }
-        }
-
-        protected override void InitRam()
-        {
-            base.InitRam();
-            // init prof-rom
-            m_romPages = new byte[16][];
-            for (int i = 0; i < m_romPages.Length; i++)
-                m_romPages[i] = new byte[0x4000];
-        }
-
-        public override void ResetState()
-        {
-            m_profPlane = 0;
-            base.ResetState();
-        }
-
-        protected override int GetRomPage()
-        {
-            int romPage = base.GetRomPage();
-            romPage |= m_profPlane << 2;
-            return romPage;
         }
     }
 }
