19dbfcf0a396660e676f00fddb5324f19c13d1b6
Alex Makeev <zxmak@mail.ru>
2013-05-24 21:43:32 +0000

    PEVO FDD refactoring/add WD Emu

--- ZXMAK2/Hardware/Evo/FddPentEvo.cs
+++ ZXMAK2/Hardware/Evo/FddPentEvo.cs
@@ -8,17 +8,12 @@ namespace ZXMAK2.Hardware.Evo
 {
     public class FddPentEvo : FddController
     {
-        private MemoryPentEvo m_memoryPentEvo;
+        private byte m_p2F, m_p4F, m_p6F, m_p8F;
+
         
         public override string Name
         {
             get { return "FDD PentEvo"; }
-        }
-
-        public override void BusInit(IBusManager bmgr)
-        {
-            m_memoryPentEvo = bmgr.FindDevice<MemoryPentEvo>();
-            base.BusInit(bmgr);
         }
 
         protected override void OnSubscribeIo(IBusManager bmgr)
@@ -27,11 +22,59 @@ namespace ZXMAK2.Hardware.Evo
             bmgr.SubscribeRdIo(0x009F, 0x001F, BusReadFdc);
             bmgr.SubscribeWrIo(0x00FF, 0x00FF, BusWriteSys);
             bmgr.SubscribeRdIo(0x00FF, 0x00FF, BusReadSys);
+
+            bmgr.SubscribeWrIo(0x00FF, 0x002F, WritePortEmu);
+            bmgr.SubscribeWrIo(0x00FF, 0x004F, WritePortEmu);
+            bmgr.SubscribeWrIo(0x00FF, 0x006F, WritePortEmu);
+            bmgr.SubscribeWrIo(0x00FF, 0x008F, WritePortEmu);
+            bmgr.SubscribeRdIo(0x00FF, 0x002F, ReadPortEmu);
+            bmgr.SubscribeRdIo(0x00FF, 0x004F, ReadPortEmu);
+            bmgr.SubscribeRdIo(0x00FF, 0x006F, ReadPortEmu);
+            bmgr.SubscribeRdIo(0x00FF, 0x008F, ReadPortEmu);
         }
 
-        public override bool IsActive
+        protected virtual void WritePortEmu(ushort addr, byte val, ref bool iorqge)
         {
-            get { return base.IsActive || (m_memoryPentEvo != null && m_memoryPentEvo.SHADOW); }
+            if (IsActive)
+            {
+                switch ((addr & 0x00FF) >> 4)
+                {
+                    case 0x02:
+                        m_p2F = val;
+                        break;
+                    case 0x04:
+                        m_p4F = val;
+                        break;
+                    case 0x06:
+                        m_p6F = val;
+                        break;
+                    case 0x08:
+                        m_p8F = val;
+                        break;
+                }
+            }
+        }
+
+        protected virtual void ReadPortEmu(ushort addr, ref byte val, ref bool iorqge)
+        {
+            if (IsActive)
+            {
+                switch ((addr & 0x00FF) >> 4)
+                {
+                    case 0x02:
+                        val = m_p2F;
+                        break;
+                    case 0x04:
+                        val = m_p4F;
+                        break;
+                    case 0x06:
+                        val = m_p6F;
+                        break;
+                    case 0x08:
+                        val = m_p8F;
+                        break;
+                }
+            }
         }
     }
 }
