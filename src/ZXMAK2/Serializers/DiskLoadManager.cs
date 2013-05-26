9f1336c6c6143b887a478552584fac48d955eec0
Alex Makeev <zxmak@mail.ru>
2013-05-26 08:52:30 +0000

    add PRO serializer (PROFI disk image 5 x 1024 sectors x 2 side x 80 cyl)

--- src/ZXMAK2/Serializers/DiskLoadManager.cs
+++ src/ZXMAK2/Serializers/DiskLoadManager.cs
@@ -8,90 +8,91 @@ using ZXMAK2.Serializers.DiskSerializers;
 
 namespace ZXMAK2.Serializers
 {
-	public class DiskLoadManager : SerializeManager
-	{
-		public DiskLoadManager(DiskImage diskImage)
-		{
-			AddSerializer(new UdiSerializer(diskImage));
-			AddSerializer(new FdiSerializer(diskImage));
-			AddSerializer(new Td0Serializer(diskImage));
-			AddSerializer(new TrdSerializer(diskImage));
-			AddSerializer(new ImgSerializer(diskImage));
-			AddSerializer(new SclSerializer(diskImage));
-			AddSerializer(new HobetaSerializer(diskImage));
+    public class DiskLoadManager : SerializeManager
+    {
+        public DiskLoadManager(DiskImage diskImage)
+        {
+            AddSerializer(new UdiSerializer(diskImage));
+            AddSerializer(new FdiSerializer(diskImage));
+            AddSerializer(new Td0Serializer(diskImage));
+            AddSerializer(new TrdSerializer(diskImage));
+            AddSerializer(new ImgSerializer(diskImage));
+            AddSerializer(new ProSerializer(diskImage));
+            AddSerializer(new SclSerializer(diskImage));
+            AddSerializer(new HobetaSerializer(diskImage));
 
-			diskImage.SaveDisk += new SaveDiskDelegate(saveDisk);
-		}
+            diskImage.SaveDisk += new SaveDiskDelegate(saveDisk);
+        }
 
-		private void saveDisk(DiskImage sender)
-		{
-			if (sender.IsWP)
-			{
-				LogAgent.Error("Write protected disk was changed! Autosave canceled");
-				return;
-			}
+        private void saveDisk(DiskImage sender)
+        {
+            if (sender.IsWP)
+            {
+                LogAgent.Error("Write protected disk was changed! Autosave canceled");
+                return;
+            }
 
-			string fileName = sender.FileName;
-			if (fileName != string.Empty)
-			{
-				FormatSerializer serializer = GetSerializer(Path.GetExtension(fileName));
-				if (serializer == null || !serializer.CanSerialize)
-					fileName = string.Empty;
-			}
-			if (fileName == string.Empty)
-			{
-				string folderName = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-				folderName = Path.Combine(folderName, "Images");
-				for (int i = 0; i < 10001; i++)
-				{
-					fileName = string.Format("zxmak2image{0:D3}{1}", i, GetDefaultExtension());
-					fileName = Path.Combine(folderName, fileName);
-					if (!File.Exists(fileName))
-						break;
-					fileName = string.Empty;
-				}
-			}
+            string fileName = sender.FileName;
+            if (fileName != string.Empty)
+            {
+                FormatSerializer serializer = GetSerializer(Path.GetExtension(fileName));
+                if (serializer == null || !serializer.CanSerialize)
+                    fileName = string.Empty;
+            }
+            if (fileName == string.Empty)
+            {
+                string folderName = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                folderName = Path.Combine(folderName, "Images");
+                for (int i = 0; i < 10001; i++)
+                {
+                    fileName = string.Format("zxmak2image{0:D3}{1}", i, GetDefaultExtension());
+                    fileName = Path.Combine(folderName, fileName);
+                    if (!File.Exists(fileName))
+                        break;
+                    fileName = string.Empty;
+                }
+            }
 
-			string msg = string.Format(
-				"Do you want to save disk changes to {0}",
-				Path.GetFileName(fileName));
+            string msg = string.Format(
+                "Do you want to save disk changes to {0}",
+                Path.GetFileName(fileName));
 
-			DlgResult qr = DialogProvider.Show(
-				msg,
-				"Attention!",
-				DlgButtonSet.YesNo,
-				DlgIcon.Question);
-			if (qr == DlgResult.Yes)
-			{
-				//if (Path.GetExtension(_fileName).ToUpper() == ".SCL")
-				//{
-				//   _fileName = Path.ChangeExtension(_fileName, ".TRD");
-				//   if (File.Exists(_fileName))
-				//      _fileName = string.Empty;
-				//}
+            DlgResult qr = DialogProvider.Show(
+                msg,
+                "Attention!",
+                DlgButtonSet.YesNo,
+                DlgIcon.Question);
+            if (qr == DlgResult.Yes)
+            {
+                //if (Path.GetExtension(_fileName).ToUpper() == ".SCL")
+                //{
+                //   _fileName = Path.ChangeExtension(_fileName, ".TRD");
+                //   if (File.Exists(_fileName))
+                //      _fileName = string.Empty;
+                //}
 
-				if (fileName == string.Empty)
-				{
-					DialogProvider.Show(
-						"Can't save disk image!\nNo space on HDD!",
-						"Warning",
-						DlgButtonSet.OK,
-						DlgIcon.Warning);
-				}
-				else
-				{
-					string folderName = Path.GetDirectoryName(fileName);
-					if (!Directory.Exists(folderName))
-						Directory.CreateDirectory(folderName);
-					sender.FileName = fileName;
-					SaveFileName(sender.FileName);
-					DialogProvider.Show(
-						string.Format("Disk image successfuly saved!\n{0}", sender.FileName),
-						"Notification",
-						DlgButtonSet.OK,
-						DlgIcon.Information);
-				}
-			}
-		}
-	}
+                if (fileName == string.Empty)
+                {
+                    DialogProvider.Show(
+                        "Can't save disk image!\nNo space on HDD!",
+                        "Warning",
+                        DlgButtonSet.OK,
+                        DlgIcon.Warning);
+                }
+                else
+                {
+                    string folderName = Path.GetDirectoryName(fileName);
+                    if (!Directory.Exists(folderName))
+                        Directory.CreateDirectory(folderName);
+                    sender.FileName = fileName;
+                    SaveFileName(sender.FileName);
+                    DialogProvider.Show(
+                        string.Format("Disk image successfuly saved!\n{0}", sender.FileName),
+                        "Notification",
+                        DlgButtonSet.OK,
+                        DlgIcon.Information);
+                }
+            }
+        }
+    }
 }
---
+++ src/ZXMAK2/Serializers/DiskSerializers/ProSerializer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ZXMAK2.Entities;
+
+
+namespace ZXMAK2.Serializers.DiskSerializers
+{
+    /// <summary>
+    /// PROFI disk image 80 track with 5 x 1024kb sectors = 819200kb
+    /// Track 000 Side 0 1,2,3,4,9
+    /// Track 001 Side 1 1,2,3,4,5
+    /// ...
+    /// Track 159 Side 1 1,2,3,4,5
+    /// </summary>
+    public class ProSerializer : FormatSerializer
+    {
+        #region Fields
+
+        private DiskImage m_diskImage;
+
+        private static byte[] s_secMap0 = new byte[] { 1, 2, 3, 4, 9 };
+        private static byte[] s_secMapX = new byte[] { 1, 2, 3, 4, 5 };
+
+        #endregion
+
+
+        #region Properties
+
+        public override string FormatExtension
+        {
+            get { return "PRO"; }
+        }
+
+        public override string FormatGroup
+        {
+            get { return "Disk images"; }
+        }
+
+        public override string FormatName
+        {
+            get { return "PRO disk image"; }
+        }
+
+        public override bool CanDeserialize
+        {
+            get { return true; }
+        }
+
+        public override bool CanSerialize
+        {
+            get { return true; }
+        }
+
+        #endregion
+
+
+        #region Public
+
+        public ProSerializer(DiskImage diskImage)
+        {
+            m_diskImage = diskImage;
+        }
+
+        public override void Deserialize(Stream stream)
+        {
+            LoadFromStream(stream);
+            m_diskImage.ModifyFlag = ModifyFlag.None;
+            m_diskImage.Present = true;
+        }
+
+        public override void Serialize(Stream stream)
+        {
+            SaveToStream(stream);
+            m_diskImage.ModifyFlag = ModifyFlag.None;
+        }
+
+        public override void SetReadOnly(bool readOnly)
+        {
+            m_diskImage.IsWP = readOnly;
+        }
+
+        public override void SetSource(string fileName)
+        {
+            m_diskImage.FileName = fileName;
+        }
+
+        #endregion Public
+
+
+        #region Private
+
+        private void LoadFromStream(Stream stream)
+        {
+            var cylSize = s_secMap0.Length * 0x400 * 2;
+            var cylCount = (int)(stream.Length / cylSize);
+            if ((stream.Length % cylSize) > 0L)
+            {
+                cylCount += 1;
+            }
+            m_diskImage.SetPhysics(cylCount, 2);
+
+            for (var c = 0; c < m_diskImage.CylynderCount; c++)
+            {
+                for (var h = 0; h < m_diskImage.SideCount; h++)
+                {
+                    var sectorList = new List<Sector>();
+                    var il = (c == 0 && h == 0) ? s_secMap0 : s_secMapX;
+                    for (var s = 0; s < il.Length; s++)
+                    {
+                        var sector = new SimpleSector(
+                            c,
+                            h,
+                            il[s],
+                            3,
+                            new byte[0x400]);
+                        stream.Read(sector.Data, 0, sector.Data.Length);
+                        sector.SetAdCrc(true);
+                        sector.SetDataCrc(true);
+                        sectorList.Add(sector);
+                    }
+                    m_diskImage
+                        .GetTrackImage(c, h)
+                        .AssignSectors(sectorList);
+                }
+            }
+            m_diskImage.ModifyFlag = ModifyFlag.None;
+        }
+
+        private void SaveToStream(Stream stream)
+        {
+            // save at least 80 cylinders
+            var cylCount = m_diskImage.CylynderCount < 80 ?
+                80 : m_diskImage.CylynderCount;
+            for (var c = 0; c < cylCount; c++)
+            {
+                for (var h = 0; h < 2; h++)
+                {
+                    var il = (c == 0 && h == 0) ? s_secMap0 : s_secMapX;
+                    for (var s = 0; s < il.Length; s++)
+                    {
+                        var buffer = new byte[0x400];
+                        if (c < m_diskImage.CylynderCount)
+                        {
+                            m_diskImage
+                                .ReadLogicalSector(c, h, il[s], buffer);
+                        }
+                        stream.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+
+        #endregion Private
+    }
+}
