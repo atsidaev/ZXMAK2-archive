95cd871f573d6f25e99c7078656ada6a80b15365
Alex Makeev <zxmak@mail.ru>
2014-04-11 01:15:18 +0000

    add Launcher

--- src/ZXMAK2/Program.cs
+++ src/ZXMAK2/Program.cs
@@ -1,63 +1,21 @@
 ﻿using System;
 using System.Windows.Forms;
-using ZXMAK2.MVP;
 using ZXMAK2.MVP.WinForms;
 
 
 namespace ZXMAK2
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			LogAgent.Start();
-            try
-            {
-                using (var presenter = new MainPresenter(new MainView(), args))
-                {
-                    presenter.Run();
-                }
-            }
-            catch (System.IO.FileNotFoundException ex)
-            {
-                LogAgent.Error(ex);
-                if (!ex.FileName.Contains("Microsoft.DirectX"))
-                {
-                    MessageBox.Show(
-                        string.Format("{0}\n\n{1}", ex.GetType(), ex.Message),
-                        "ZXMAK2",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
-                else
-                {
-                    MessageBox.Show(
-                        string.Format(
-                        "{0}\n\n{1}\n\nPlease install DirectX End-User Runtime and try again!", ex.GetType(), ex.Message),
-                        "ZXMAK2",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
-            }
-            catch (Exception ex)
-            {
-                LogAgent.Error(ex);
-                MessageBox.Show(
-                    string.Format("{0}\n\n{1}", ex.GetType(), ex.Message),
-                    "ZXMAK2",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-            }
-			finally
-			{
-				LogAgent.Finish();
-			}
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Launcher.Run<MainView>(args);
+        }
+    }
 }
