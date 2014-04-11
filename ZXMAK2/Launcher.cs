fc26d25e42eeeaff1b545680043c593c7128ef42
Alex Makeev <zxmak@mail.ru>
2014-04-11 01:15:18 +0000

    add Launcher

---
+++ ZXMAK2/Launcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ZXMAK2.MVP.Interfaces;
+using ZXMAK2.MVP;
+
+
+namespace ZXMAK2
+{
+    public class Launcher
+    {
+        public static void Run<T>(string[] args)
+            where T : IMainView, new()
+        {
+            LogAgent.Start();
+            try
+            {
+                var view = new T();
+                using (var presenter = new MainPresenter(view, args))
+                {
+                    presenter.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogAgent.Error(ex);
+                try
+                {
+                    DialogService.Show(
+                        string.Format("{0}\n\n{1}", ex.GetType(), ex.Message),
+                        "ZXMAK2",
+                        DlgButtonSet.OK,
+                        DlgIcon.Error);
+                }
+                catch (Exception ex2)
+                {
+                    LogAgent.Error(ex2);
+                }
+            }
+            finally
+            {
+                LogAgent.Finish();
+            }
+        }
+    }
+}
