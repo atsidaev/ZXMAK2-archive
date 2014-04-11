42e063494bd3181ec734358f945d024f0939c160
Alex Makeev <zxmak@mail.ru>
2014-04-11 00:02:27 +0000

    add fpsmonitor to project; move to Engine folder

---
+++ src/ZXMAK2/Engine/FpsMonitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace ZXMAK2.Engine
+{
+    public class FpsMonitor
+    {
+        private Stopwatch m_watch;
+        private int m_frameCounter;
+
+        public double Value { get; private set; }
+
+
+        public FpsMonitor()
+        {
+            Value = 0;
+            m_frameCounter = 0;
+            m_watch = Stopwatch.StartNew();
+        }
+
+        public void Frame()
+        {
+            var time = m_watch.ElapsedTicks;
+            m_frameCounter++;
+            if (time >= Stopwatch.Frequency/3)
+            {
+                m_watch.Stop();
+                m_watch.Reset();
+                m_watch.Start();
+                Value = m_frameCounter * (double)Stopwatch.Frequency / time;
+                m_frameCounter = 0;
+            }
+        }
+    }
+}
