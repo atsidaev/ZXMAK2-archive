b6afb765c9cd52f27e45b445aa6d8cbd56a8abf6
Alex Makeev <zxmak@mail.ru>
2014-04-19 21:24:00 +0000

    Add keyboard mapping; remove keyboard dependency on MDX

--- ZXMAK2.XNA4/Properties/Resources.Designer.cs
+++ ZXMAK2.XNA4/Properties/Resources.Designer.cs
@@ -61,6 +61,29 @@ namespace ZXMAK2.XNA4.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;
+        ///&lt;KeyboardMap Type=&quot;Microsoft.Xna.Framework.Input.Keys&quot;&gt;
+        ///    &lt;Key Name=&quot;D1&quot; Value=&quot;D1&quot; /&gt;
+        ///    &lt;Key Name=&quot;D2&quot; Value=&quot;D2&quot; /&gt;
+        ///    &lt;Key Name=&quot;D3&quot; Value=&quot;D3&quot; /&gt;
+        ///    &lt;Key Name=&quot;D4&quot; Value=&quot;D4&quot; /&gt;
+        ///    &lt;Key Name=&quot;D5&quot; Value=&quot;D5&quot; /&gt;
+        ///    &lt;Key Name=&quot;D6&quot; Value=&quot;D6&quot; /&gt;
+        ///    &lt;Key Name=&quot;D7&quot; Value=&quot;D7&quot; /&gt;
+        ///    &lt;Key Name=&quot;D8&quot; Value=&quot;D8&quot; /&gt;
+        ///    &lt;Key Name=&quot;D9&quot; Value=&quot;D9&quot; /&gt;
+        ///    &lt;Key Name=&quot;D0&quot; Value=&quot;D0&quot; /&gt;
+        ///    &lt;Key Name=&quot;Q&quot; Value=&quot;Q&quot; /&gt;
+        ///    &lt;Key Name=&quot;W&quot; Value=&quot;W&quot; /&gt;
+        ///    &lt;Key N [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string KeyboardMap {
+            get {
+                return ResourceManager.GetString("KeyboardMap", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] SansSerifBold {
---
+++ ZXMAK2/Entities/KeyboardMapper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZXMAK2.Interfaces;
+using System.Xml;
+
+namespace ZXMAK2.Entities
+{
+    public class KeyboardStateMapper<T>
+        where T : struct
+    {
+        private readonly Dictionary<Key, T> m_map = new Dictionary<Key, T>();
+
+        public IEnumerable<Key> Keys { get { return m_map.Keys; } }
+
+
+        public T this[Key key]
+        {
+            get { return m_map[key]; }
+        }
+
+
+        #region Map
+
+        public void LoadMap(string fileName)
+        {
+            var xml = new XmlDocument();
+            xml.Load(fileName);
+            LoadMapFromXml(xml);
+        }
+        
+        public void LoadMapFromString(string xmlString)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(xmlString);
+            LoadMapFromXml(xml);
+        }
+        
+        public void LoadMapFromXml(XmlNode xml)
+        {
+            m_map.Clear();
+            try
+            {
+                foreach (XmlNode node in xml.SelectNodes("/KeyboardMap/Key[count(@Name)>0 and count(@Value)>0]"))
+                {
+                    var name = node.Attributes["Name"].Value.Trim();
+                    var value = node.Attributes["Value"].Value.Trim();
+                    if (name == string.Empty || value == string.Empty)
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        Key emuKey = (Key)Enum.Parse(typeof(Key), name);
+                        T devKey = (T)Enum.Parse(typeof(T), value);
+                        if (m_map.ContainsKey(emuKey))
+                        {
+                            LogAgent.Warn(
+                                "Duplicate keyboard mapping \"{0}\", \"{1}\"",
+                                name,
+                                value);
+                            continue;
+                        }
+                        m_map[emuKey] = devKey;
+                    }
+                    catch (Exception ex)
+                    {
+                        LogAgent.Warn(
+                            "Invalid keyboard mapping \"{0}\", \"{1}\" ({2})",
+                            name,
+                            value,
+                            ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogAgent.Error(ex);
+            }
+        }
+
+        #endregion Map
+    }
+}
