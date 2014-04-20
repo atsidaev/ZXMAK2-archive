a70097ecbfd114aeec8dc362d8f16f97462aeef4
Alex Makeev <zxmak@mail.ru>
2014-04-20 16:07:45 +0000

    refactoring

--- src/Presentation/Interfaces/IViewResolver.cs
+++ src/Presentation/Interfaces/IViewResolver.cs
@@ -2,7 +2,7 @@
 using ZXMAK2.Dependency;
 
 
-namespace ZXMAK2.Interfaces
+namespace ZXMAK2.Presentation.Interfaces
 {
     public interface IViewResolver
     {
--- src/ZXMAK2/Launcher.cs
+++ src/ZXMAK2/Launcher.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using ZXMAK2.MVP.Interfaces;
 using ZXMAK2.MVP;
 using ZXMAK2.Interfaces;
-using System.Collections.Generic;
 using ZXMAK2.Dependency;
+using ZXMAK2.Presentation.Interfaces;
 
 
 namespace ZXMAK2
