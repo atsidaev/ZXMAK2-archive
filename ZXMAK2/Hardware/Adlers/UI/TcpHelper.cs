a094c9b9812a3530a8cb1e013aa576c9d75a7797
Adlers <adlers@none.none>
2014-12-15 11:20:31 +0000

    Assembler: - get Z80 cross compiler dll from net(new class TcpHelper) - bugfixing

---
+++ ZXMAK2/Hardware/Adlers/UI/TcpHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZXMAK2.Hardware.Adlers.UI
+{
+    public partial class TcpHelper : Form
+    {
+        private static WebClient m_client;
+
+        public TcpHelper()
+        {
+            InitializeComponent();
+
+            m_client = new WebClient();
+            m_client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+        }
+
+        private void checkBoxIsProxy_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isChecked = checkBoxIsProxy.Checked;
+
+            this.textBoxProxyAdress.Enabled = isChecked;
+            this.textBoxProxyPort.Enabled = isChecked;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            m_client.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            m_client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+            if(checkBoxIsProxy.Checked)
+            {
+                WebProxy proxy = new WebProxy("http://" + this.textBoxProxyAdress.Text.Trim() + ":" + this.textBoxProxyPort.Text.Trim() + "/",true);
+                m_client.Proxy = proxy;
+            }
+
+            m_client.DownloadFileAsync(new Uri(@"http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pasmo2&DownloadId=991511&FileTime=130631149681430000&Build=20959"), "Pasmo2.dll");
+        }
+
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progressBarDownloadStatus.Value = e.ProgressPercentage;
+        }
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download completed!");
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (m_client.IsBusy)
+                m_client.CancelAsync();
+
+            this.Hide();
+        }
+    }
+}
