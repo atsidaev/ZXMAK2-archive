94a3c88d4ebf091faab6525dc9064e478139be63
Alex Makeev <zxmak@mail.ru>
2015-01-04 17:48:48 +0000

    move UserMessage to ZXMAK2.Host (step 2 of 2)

--- src/ZXMAK2.Host/UserMessage.cs
+++ src/ZXMAK2.Host/UserMessage.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ZXMAK2.Interfaces;
 using ZXMAK2.Dependency;
 using ZXMAK2.Host.Entities;
 using ZXMAK2.Host.Interfaces;
 
-namespace ZXMAK2.Presentation
+
+namespace ZXMAK2.Host
 {
     public class UserMessage : IUserMessage
     {
