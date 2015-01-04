82fcec1de88cd472f82b800f33190f8ca56990ad
Alex Makeev <zxmak@mail.ru>
2015-01-04 17:15:53 +0000

    rename WinForms & XNA4 to Host.WinForms & Host.Xna4 (step 2 of 4)

--- src/ZXMAK2.XNA4/Properties/Resources.Designer.cs
+++ src/ZXMAK2.XNA4/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZXMAK2.XNA4.Properties {
+namespace ZXMAK2.Host.Xna4.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ZXMAK2.XNA4.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZXMAK2.XNA4.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZXMAK2.Host.Xna4.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
