e2da3d2ceb448c7ff7f688149dee671bf862d3fd
Alex Makeev <zxmak@mail.ru>
2015-01-04 18:49:53 +0000

    rename MDX to Mdx

--- src/ZXMAK2.Host.WinForms/MainView.cs
+++ src/ZXMAK2.Host.WinForms/MainView.cs
@@ -13,7 +13,7 @@ using ZXMAK2.Entities;
 using ZXMAK2.MVP.Interfaces;
 using ZXMAK2.Dependency;
 using ZXMAK2.Host.Interfaces;
-using ZXMAK2.Host.WinForms.MDX;
+using ZXMAK2.Host.WinForms.Mdx;
 
 
 namespace ZXMAK2.Host.WinForms
