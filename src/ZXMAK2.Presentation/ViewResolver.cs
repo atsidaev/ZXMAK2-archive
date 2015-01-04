3bbc67976e220d03ec41b638eb80a435e509a86b
Alex Makeev <zxmak@mail.ru>
2015-01-04 22:50:14 +0000

    temp checkin moving interfaces

--- src/ZXMAK2.Presentation/ViewResolver.cs
+++ src/ZXMAK2.Presentation/ViewResolver.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ZXMAK2.Dependency;
-using ZXMAK2.MVP.Interfaces;
+using ZXMAK2.Host.Presentation.Interfaces;
 
 
 namespace ZXMAK2.Presentation
