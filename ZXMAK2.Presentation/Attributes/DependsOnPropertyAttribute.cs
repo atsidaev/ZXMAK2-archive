a5bcb724038d24ad592b78011429ce42782906c9
Alex Makeev <zxmak@mail.ru>
2015-01-09 23:03:00 +0000

    add ViewModelBase

---
+++ ZXMAK2.Presentation/Attributes/DependsOnPropertyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZXMAK2.Presentation.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public class DependsOnPropertyAttribute : Attribute
+    {
+        public DependsOnPropertyAttribute(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException("propertyName");
+            }
+            PropertyName = propertyName;
+        }
+
+        public string PropertyName { get; private set; }
+    }
+}
