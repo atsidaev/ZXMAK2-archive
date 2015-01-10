689067c4badcc31c5f0915272440b045d0963f8c
Alex Makeev <zxmak@mail.ru>
2015-01-10 09:54:38 +0000

    replace OpenFile/SaveFile with services; remove UserHelpProxy

---
+++ src/ZXMAK2.Host/Interfaces/ISaveFileDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZXMAK2.Host.Entities;
+
+namespace ZXMAK2.Host.Interfaces
+{
+    public interface ISaveFileDialog : IDisposable
+    {
+        string Title { get; set; }
+        string Filter { get; set; }
+        string DefaultExt { get; set; }
+        string FileName { get; set; }
+        bool OverwritePrompt { get; set; }
+
+        DlgResult ShowDialog(object owner);
+    }
+}
