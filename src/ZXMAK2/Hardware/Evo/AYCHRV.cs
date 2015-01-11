dd7f0730d425a901bb4ba0c448ecd2365a678311
Alex Makeev <zxmak@mail.ru>
2015-01-11 04:52:27 +0000

    fix PENTEVO AY8910-IRB issue (freeze on select 48K)

---
+++ src/ZXMAK2/Hardware/Evo/AYCHRV.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZXMAK2.Hardware.General;
+
+namespace ZXMAK2.Hardware.Evo
+{
+    public class AYCHRV : AY8910
+    {
+        public AYCHRV()
+        {
+            UpdateIRB += OnUpdateIrb;
+        }
+
+        public override string Name
+        {
+            get { return "AY8910-CHRV"; }
+        }
+
+        private void OnUpdateIrb(AY8910 sender, AyPortState state)
+        {
+            if (!state.DirOut)
+            {
+                state.InState = 0xFE; /*always ready??? Alone Coder us0.36.6*/
+            }
+        }
+    }
+}
