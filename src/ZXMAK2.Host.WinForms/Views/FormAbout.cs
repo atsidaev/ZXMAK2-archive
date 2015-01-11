4a21e8c68887c954c9c1f52fb8e652b2465c9153
Alex Makeev <zxmak@mail.ru>
2015-01-11 06:44:45 +0000

    fix namespace

--- src/ZXMAK2.Host.WinForms/Views/FormAbout.cs
+++ src/ZXMAK2.Host.WinForms/Views/FormAbout.cs
@@ -5,9 +5,10 @@ using ZXMAK2.Resources;
 using ZXMAK2.Presentation.Interfaces;
 using ZXMAK2.Host.Interfaces;
 using ZXMAK2.Host.Presentation.Interfaces;
+using ZXMAK2.Controls;
 
 
-namespace ZXMAK2.Controls
+namespace ZXMAK2.Host.WinForms.Views
 {
     public class FormAbout : FormView, IAboutView
 	{
