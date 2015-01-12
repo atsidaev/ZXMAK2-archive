9a50630f835de9319bbda7dbc714deba79f0da07
Alex Makeev <zxmak@mail.ru>
2015-01-12 02:59:05 +0000

    add IVirtualMachine and IBus for machine settings interface

---
+++ src/ZXMAK2.Engine/Interfaces/IVirtualMachine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ZXMAK2.Engine.Interfaces
+{
+    public interface IVirtualMachine : IDisposable
+    {
+        bool IsRunning { get; }
+        IBus Bus { get; }
+        
+        void DoRun();
+        void DoStop();
+
+        void DoReset();
+        void DoNmi();
+
+        void SaveConfig();
+    }
+}
