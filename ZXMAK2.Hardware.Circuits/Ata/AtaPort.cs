154ce390f83b7ac68dcd2ca10fa26b0fbcb891f5
Alex Makeev <zxmak@mail.ru>
2015-01-13 22:13:48 +0000

    fix log messages

--- ZXMAK2.Hardware.Circuits/Ata/AtaPort.cs
+++ ZXMAK2.Hardware.Circuits/Ata/AtaPort.cs
@@ -29,7 +29,7 @@ namespace ZXMAK2.Hardware.Circuits.Ata
 
         public void Reset()
         {
-            //Logger.Debug("AtaPort.reset");
+            //Logger.Debug("AtaPort.Reset");
             Devices[0].reset(RESET_TYPE.RESET_HARD);
             Devices[1].reset(RESET_TYPE.RESET_HARD);
         }
@@ -37,27 +37,27 @@ namespace ZXMAK2.Hardware.Circuits.Ata
         public byte Read(AtaReg n_reg)
         {
             byte result = (byte)(Devices[0].read(n_reg) & Devices[1].read(n_reg));
-            //Logger.Debug("AtaPort.read({0}) = 0x{1:X2}", n_reg, result);
+            //Logger.Debug("AtaPort.Read({0}) = 0x{1:X2}", n_reg, result);
             return result;
         }
 
         public UInt16 ReadData()
         {
             UInt16 result = (UInt16)(Devices[0].read_data() & Devices[1].read_data());
-            //Logger.Debug("AtaPort.read_data() = 0x{0:X4}", result);
+            //Logger.Debug("AtaPort.ReadData() = 0x{0:X4}", result);
             return result;
         }
 
         public void Write(AtaReg n_reg, byte data)
         {
-            //Logger.Debug("AtaPort.write({0}, 0x{1:X2})", n_reg, data);
+            //Logger.Debug("AtaPort.Write({0}, 0x{1:X2})", n_reg, data);
             Devices[0].write(n_reg, data);
             Devices[1].write(n_reg, data);
         }
 
         public void WriteData(UInt16 data)
         {
-            //Logger.Debug("AtaPort.write_data(0x{0:X4})", data);
+            //Logger.Debug("AtaPort.WriteData(0x{0:X4})", data);
             Devices[0].write_data(data);
             Devices[1].write_data(data);
         }
@@ -65,7 +65,7 @@ namespace ZXMAK2.Hardware.Circuits.Ata
         public byte GetIntRq()
         {
             byte result = (byte)(Devices[0].read_intrq() & Devices[1].read_intrq());
-            //Logger.Debug("AtaPort.read_intrq() = 0x{0:X2}", result);
+            //Logger.Debug("AtaPort.GetIntRq() = 0x{0:X2}", result);
             return result;
         }
 
