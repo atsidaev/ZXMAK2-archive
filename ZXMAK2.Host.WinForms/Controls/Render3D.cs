cd6de1195a873b8df135ffc0f321f467007bdcbb
Alex Makeev <zxmak@mail.ru>
2015-01-16 21:45:49 +0000

    fix Direct3D init; Render3D refactoring

--- ZXMAK2.Host.WinForms/Controls/Render3D.cs
+++ ZXMAK2.Host.WinForms/Controls/Render3D.cs
@@ -2,6 +2,7 @@
 /// Author: Alex Makeev
 /// Date: 26.03.2008
 using System;
+using System.Linq;
 using System.Drawing;
 using System.Windows.Forms;
 using Microsoft.DirectX.Direct3D;
@@ -10,58 +11,223 @@ using Microsoft.DirectX.Direct3D;
 
 namespace ZXMAK2.Host.WinForms.Controls
 {
-	public class Render3D : Control
-	{
-		protected readonly object SyncRoot = new object();
+    public class Render3D : Control
+    {
+        #region Fields
 
-		private int m_frameCounter = 0;
-		private bool m_vBlankSync = false;
-		protected Device D3D = null;
-		private PresentParameters m_presentParams = new PresentParameters();
+        private readonly PresentParameters m_presentParams = new PresentParameters();
+        protected readonly object SyncRoot = new object();
+        protected Device D3D;
 
-		public event EventHandler DeviceReset;
+        #endregion Fields
 
-		public Render3D()
-		{
-			SetStyle(ControlStyles.Opaque | ControlStyles.UserPaint | ControlStyles.ResizeRedraw, true);
-		}
 
-		public void InitWnd()
-		{
-			lock (SyncRoot)
-			{
-				if (D3D == null)
-					init();
-			}
-		}
+        public Render3D()
+        {
+            SetStyle(ControlStyles.Opaque | ControlStyles.UserPaint | ControlStyles.ResizeRedraw, true);
+        }
 
-		public void FreeWnd()
-		{
-			lock (SyncRoot)
-			{
-				if (D3D != null)
-					free();
-			}
-		}
 
-		protected override void OnPaint(PaintEventArgs e)
-		{
-			try
-			{
-				if (D3D != null)
-				{
-					RenderScene();
-				}
-				else
-				{
+        #region Properties
+
+        public event EventHandler DeviceReset;
+
+        #endregion Properties
+
+
+        #region Public
+
+        public void InitWnd()
+        {
+            lock (SyncRoot)
+            {
+                if (D3D != null)
+                {
+                    return;
+                }
+                InitDevice();
+            }
+        }
+
+        public void FreeWnd()
+        {
+            lock (SyncRoot)
+            {
+                if (D3D == null)
+                {
+                    return;
+                }
+                FreeDevice();
+            }
+        }
+
+        #endregion Public
+
+
+        #region Private
+
+        private void InitDevice()
+        {
+            try
+            {
+                lock (SyncRoot)
+                {
+                    D3D = CreateDirect3D();
+                    D3D.DeviceResizing += new System.ComponentModel.CancelEventHandler(Device_OnDeviceResizing);
+                    D3D.DeviceReset += new EventHandler(Device_OnDeviceReset);
+                    OnCreateDevice();
+                    RenderScene();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private void FreeDevice()
+        {
+            try
+            {
+                lock (SyncRoot)
+                {
+                    if (D3D == null)
+                    {
+                        return;
+                    }
+                    OnDestroyDevice();
+                    D3D.Dispose();
+                    D3D = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private void ResetDevice()
+        {
+            try
+            {
+                lock (SyncRoot)
+                {
+                    if (D3D == null)
+                    {
+                        return;
+                    }
+                    ConfigureDeviceParams(false);
+                    D3D.Reset(m_presentParams);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private void Device_OnDeviceReset(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DeviceReset == null)
+                {
+                    return;
+                }
+                DeviceReset(this, EventArgs.Empty);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private void Device_OnDeviceResizing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            try
+            {
+                if (D3D != null)
+                {
+                    RenderScene();
+                }
+                else
+                {
                     RenderError(e, "Direct3D initialization failed!\nProbably DirectX 9 (June 2010) is not installed");
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            ResetDevice();
+        }
+
+        protected virtual void OnCreateDevice()
+        {
+        }
+
+        protected virtual void OnDestroyDevice()
+        {
+        }
+
+        protected virtual void OnRenderScene()
+        {
+        }
+
+        protected void RenderScene()
+        {
+            try
+            {
+                lock (SyncRoot)
+                {
+                    if (D3D == null ||
+                        !Visible ||
+                        ClientSize.Width <= 0 ||
+                        ClientSize.Height <= 0)
+                    {
+                        return;
+                    }
+                    int resultCodeInt;
+                    D3D.CheckCooperativeLevel(out resultCodeInt);
+                    var resultCode = (ResultCode)resultCodeInt;
+                    switch (resultCode)
+                    {
+                        case ResultCode.DeviceNotReset:
+                            //LogAgent.Debug("DeviceNotReset");
+                            ResetDevice();
+                            break;
+                        case ResultCode.DeviceLost:
+                            //LogAgent.Debug("DeviceLost");
+                            // e.g. aquired by other app
+                            break;
+                        case ResultCode.Success:
+                            D3D.Clear(ClearFlags.Target, Color.Black, 1, 0);
+                            D3D.BeginScene();
+                            OnRenderScene();
+                            D3D.EndScene();
+                            D3D.Present();
+                            break;
+                        default:
+                            Logger.Info("CheckCooperativeLevel = {0}", resultCode);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
 
         protected void RenderError(PaintEventArgs e, string message)
         {
@@ -77,212 +243,70 @@ namespace ZXMAK2.Host.WinForms.Controls
             }
         }
 
-		private void init()
-		{
-			try
-			{
-				lock (SyncRoot)
-				{
-					int adapter = Manager.Adapters.Default.Adapter;
-					Caps caps = Manager.GetDeviceCaps(adapter, DeviceType.Hardware);
-					CreateFlags flags = CreateFlags.MultiThreaded;
-					if (caps.DeviceCaps.SupportsHardwareTransformAndLight)
-						flags |= CreateFlags.HardwareVertexProcessing;
-					else
-						flags |= CreateFlags.SoftwareVertexProcessing;
+        #endregion Private
 
-					m_presentParams.Windowed = true;
-					m_presentParams.PresentationInterval =
-						m_vBlankSync ? PresentInterval.One : PresentInterval.Immediate;
-					m_presentParams.SwapEffect =
-						m_vBlankSync ? SwapEffect.Flip : SwapEffect.Discard;
-					m_presentParams.BackBufferCount = 1;
-					m_presentParams.BackBufferFormat = Format.A8R8G8B8;
-					m_presentParams.BackBufferWidth = ClientSize.Width > 0 ? ClientSize.Width : 1;
-					m_presentParams.BackBufferHeight = ClientSize.Height > 0 ? ClientSize.Height : 1;
-					m_presentParams.EnableAutoDepthStencil = false;
 
-					D3D = new Device(
-						adapter,
-						DeviceType.Hardware,
-						this.Handle,
-						flags,
-						m_presentParams);
-					D3D.DeviceResizing += new System.ComponentModel.CancelEventHandler(D3D_DeviceResizing);
-					D3D.DeviceReset += new EventHandler(D3D_DeviceReset);
-					OnCreateDevice();
-					RenderScene();
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
+        #region Direct3D Initialization
 
-		private void free()
-		{
-			try
-			{
-				lock (SyncRoot)
-				{
-					if (D3D != null)
-					{
-						OnDestroyDevice();
-						D3D.Dispose();
-						D3D = null;
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
+        private int GetAdapterId()
+        {
+            var screen = Screen.FromControl(this);
+            var adapterInfo = Manager.Adapters
+                .OfType<AdapterInformation>()
+                .FirstOrDefault(ai => ai.Information.DeviceName == screen.DeviceName);
+            adapterInfo = adapterInfo ?? Manager.Adapters.Default;
+            return adapterInfo.Adapter;
+        }
 
-		private void reset()
-		{
-			try
-			{
-				lock (SyncRoot)
-				{
-					if (D3D == null)
-						return;
+        private void ConfigureDeviceParams(bool vBlankSync)
+        {
+            m_presentParams.Windowed = true;
+            m_presentParams.PresentationInterval =
+                vBlankSync ? PresentInterval.One : PresentInterval.Immediate;
+            m_presentParams.SwapEffect =
+                vBlankSync ? SwapEffect.Flip : SwapEffect.Discard;
+            m_presentParams.BackBufferCount = 1;
+            m_presentParams.BackBufferFormat = Format.A8R8G8B8;
+            m_presentParams.BackBufferWidth = ClientSize.Width > 0 ? ClientSize.Width : 1;
+            m_presentParams.BackBufferHeight = ClientSize.Height > 0 ? ClientSize.Height : 1;
+            m_presentParams.EnableAutoDepthStencil = false;
+        }
 
-					m_presentParams.Windowed = true;
-					m_presentParams.PresentationInterval =
-						m_vBlankSync ? PresentInterval.One : PresentInterval.Immediate;
-					m_presentParams.SwapEffect =
-						m_vBlankSync ? SwapEffect.Flip : SwapEffect.Discard;
-					m_presentParams.BackBufferCount = 1;
-					m_presentParams.BackBufferFormat = Format.A8R8G8B8;
-					m_presentParams.BackBufferWidth = ClientSize.Width > 0 ? ClientSize.Width : 1;
-					m_presentParams.BackBufferHeight = ClientSize.Height > 0 ? ClientSize.Height : 1;
-					m_presentParams.EnableAutoDepthStencil = false;
+        private Device CreateDirect3D()
+        {
+            ConfigureDeviceParams(false);
+            var adapterId = GetAdapterId();
+            var caps = Manager.GetDeviceCaps(adapterId, DeviceType.Hardware);
+            var flags = CreateFlags.MultiThreaded;
+            if (caps.DeviceCaps.SupportsHardwareTransformAndLight)
+                flags |= CreateFlags.HardwareVertexProcessing;
+            else
+                flags |= CreateFlags.SoftwareVertexProcessing;
+            return new Device(
+                adapterId,
+                DeviceType.Hardware,
+                this.Handle,
+                flags,
+                m_presentParams);
+        }
 
-					D3D.Reset(m_presentParams);
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
+        #endregion Direct3D Initialization
 
-		private void D3D_DeviceReset(object sender, EventArgs e)
-		{
-			try
-			{
-				if (DeviceReset != null)
-					DeviceReset(this, EventArgs.Empty);
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
 
-		private void D3D_DeviceResizing(object sender, System.ComponentModel.CancelEventArgs e)
-		{
-			e.Cancel = true;
-		}
+        #region WndProc
 
-		protected override void OnResize(EventArgs e)
-		{
-			base.OnResize(e);
-			reset();
-		}
+        // Disable 'Ding' sound on Alt+Enter
+        protected const int WM_SYSCHAR = 0x106;
 
-		protected virtual void OnCreateDevice()
-		{
-		}
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_SYSCHAR && (int)m.WParam == 13)
+            {
+                return;
+            }
+            base.WndProc(ref m);
+        }
 
-		protected virtual void OnDestroyDevice()
-		{
-		}
-
-		protected virtual void OnRenderScene()
-		{
-		}
-
-		protected void RenderScene()
-		{
-			try
-			{
-				lock (SyncRoot)
-				{
-					if (D3D != null && Visible && ClientSize.Width > 0 && ClientSize.Height > 0)
-					{
-						int resultCodeInt;
-						D3D.CheckCooperativeLevel(out resultCodeInt);
-						ResultCode resultCode = (ResultCode)resultCodeInt;
-						switch (resultCode)
-						{
-							case ResultCode.DeviceNotReset:
-								//LogAgent.Debug("DeviceNotReset");
-								reset();
-								break;
-							case ResultCode.DeviceLost:
-								//LogAgent.Debug("DeviceLost");
-								// e.g. aquired by other app
-								break;
-							case ResultCode.Success:
-								D3D.Clear(ClearFlags.Target, Color.Black, 1, 0);
-								D3D.BeginScene();
-								OnRenderScene();
-								D3D.EndScene();
-								D3D.Present();
-								break;
-							default:
-								Logger.Info("CheckCooperativeLevel = {0}", resultCode);
-								break;
-						}
-						m_frameCounter++;
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.Error(ex);
-			}
-		}
-
-		public int FrameCounter { get { return m_frameCounter; } }
-		public int FrameRate
-		{
-			get
-			{
-				lock (SyncRoot)
-					if (D3D != null)
-						return D3D.DisplayMode.RefreshRate;
-				return 0;
-			}
-		}
-
-        //public bool VBlankSync
-        //{
-        //    get { return m_vBlankSync; }
-        //    set
-        //    {
-        //        if (value != m_vBlankSync)
-        //        {
-        //            m_vBlankSync = value;
-        //            reset();
-        //        }
-        //    }
-        //}
-
-		// Disable 'Ding' sound on Alt+Enter
-		protected const int WM_SYSCHAR = 0x106;
-		protected override void WndProc(ref Message m)
-		{
-			if (m.Msg == WM_SYSCHAR)
-			{
-				if ((int)m.WParam == 13)
-					return;
-
-			}
-			base.WndProc(ref m);
-		}
-	}
+        #endregion WndProc
+    }
 }
