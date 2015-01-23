ede23339365c797cf985ee1543c1931e9d59b5ee
Alex Makeev <zxmak@mail.ru>
2015-01-23 10:34:42 +0000

    This using directive is not required

--- src/ZXMAK2.Hardware.Adlers/Views/GraphicsEditor.cs
+++ src/ZXMAK2.Hardware.Adlers/Views/GraphicsEditor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using ZXMAK2.Engine.Interfaces;
 using ZXMAK2.Hardware.Adlers.Core;
