e8b584f658d9f81f450301c39b43e73c3367cc8a
Adlers <adlers@none.none>
2015-01-25 12:06:35 +0000

    Debugger: - assembler dll rebuild(ported to GNU GCC)

--- ZXMAK2.Hardware.Adlers/Views/TcpHelper.cs
+++ ZXMAK2.Hardware.Adlers/Views/TcpHelper.cs
@@ -43,7 +43,7 @@ namespace ZXMAK2.Hardware.Adlers.Views
                 labelStatusText.Text = "Downloading...";
                 buttonStart.Enabled = false;
 
-                m_client.DownloadFileAsync(new Uri(@"http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pasmo2&DownloadId=1421719&FileTime=130661256882600000&Build=20959"), "Pasmo2.dll");
+                m_client.DownloadFileAsync(new Uri(@"http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pasmo2&DownloadId=1422898&FileTime=130666609839170000&Build=20959"), "Pasmo2.dll");
             }
             catch(Exception tcpException)
             {
