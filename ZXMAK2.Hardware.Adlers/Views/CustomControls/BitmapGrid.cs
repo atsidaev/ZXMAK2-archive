13cecabc2180eb1d94f2e51f44a0b9af1e02b15a
Adlers <adlers@none.none>
2015-04-23 13:59:34 +0000

    Graphics editor: - bugs in grid editor corrected

--- ZXMAK2.Hardware.Adlers/Views/CustomControls/BitmapGrid.cs
+++ ZXMAK2.Hardware.Adlers/Views/CustomControls/BitmapGrid.cs
@@ -41,6 +41,10 @@ namespace ZXMAK2.Hardware.Adlers.Views.CustomControls
 
         public int getGridBit(int XBit, int YBit)
         {
+            if (XBit > X_BIT_COUNT - 1)
+                XBit = X_BIT_COUNT - 1;
+            if (YBit > Y_BIT_COUNT - 1)
+                YBit = Y_BIT_COUNT - 1;
             return XBit + YBit * X_BIT_COUNT;
         }
         public bool getGridBitValue(int XBit, int YBit)
@@ -82,6 +86,32 @@ namespace ZXMAK2.Hardware.Adlers.Views.CustomControls
             return Y_BIT_COUNT;
         }
 
+        public int getClickedPixel(MouseEventArgs e)
+        {
+            int PixelWidth = this.Width / X_BIT_COUNT;
+            decimal DeltaWidth = this.Width % X_BIT_COUNT;
+            int PixelHeight = this.Height / Y_BIT_COUNT;
+
+            int pixelClicked = getGridBit(e.X / PixelWidth, e.Y / PixelHeight);
+            int X = pixelClicked / 8;
+            int Y = pixelClicked % 8;
+
+            double DeltaHeight = e.X - (pixelClicked*8);
+            if (DeltaHeight == 0 && Y > 0)
+            {
+                //correction of clicked cell
+                Y--;
+                pixelClicked--;
+            }
+
+            //Logger.Debug("DeltaHeight: " + DeltaHeight.ToString());
+
+            _gridBits[X][Y] = !_gridBits[X][Y];
+
+            //_needRepaint = true;
+            return pixelClicked;
+        }
+
         public void Draw(PaintEventArgs e)
         {
             if (_gridBits == null)
@@ -97,8 +127,8 @@ namespace ZXMAK2.Hardware.Adlers.Views.CustomControls
                 rect.Size = new Size(bitWidth, bitHeight);
 
                 int arrBitmapCounter = 0;
-                int startX = 2; // grid margin
-                int startY = 2; // grid margin
+                int startX = 1; // grid margin
+                int startY = 1; // grid margin
                 this.Width = bitWidth * X_BIT_COUNT + 8;
                 this.Height = bitHeight * Y_BIT_COUNT + 8;
                 for (int counterY = 0; counterY < Y_BIT_COUNT; counterY++)
@@ -126,19 +156,6 @@ namespace ZXMAK2.Hardware.Adlers.Views.CustomControls
         {
             base.OnPaint(e);
             this.Draw(e);
-        }
-
-        public int getClickedPixel(MouseEventArgs e)
-        {
-            int pixelClicked = getGridBit(e.X / (this.Width / X_BIT_COUNT), e.Y / (this.Height / Y_BIT_COUNT));
-            bool setValue = !_gridBits[pixelClicked / 8][pixelClicked % 8];
-
-            _gridBits[pixelClicked / 8][pixelClicked % 8] = setValue;
-
-            //_needRepaint = true;
-            this.Draw(null);
-
-            return pixelClicked;
         }
     }
 }
