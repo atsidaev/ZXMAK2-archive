45d7c3d192dbf175387a0cdebf82e98f557b505a
Alex Makeev <zxmak@mail.ru>
2015-06-21 02:30:04 +0000

    sprinter ula & mmu optimizations

--- ZXMAK2.Hardware/Sprinter/SprinterULA.cs
+++ ZXMAK2.Hardware/Sprinter/SprinterULA.cs
@@ -59,16 +59,29 @@ namespace ZXMAK2.Hardware.Sprinter
 
         private int TXTPaletteToRGB(int attrib, int pix, int flash)
         {
-            //flash = 1 = on
-            //pix = 1 = on
-            int _Red, _Green, _Blue;
-            byte num = (byte)(flash * 2 + pix);
-            _Red = m_vram[(attrib & 0xf0) >> 4][((attrib & 0x0f)*1024)+ 0x03f0 + num * 4];
-            _Green = m_vram[(attrib & 0xf0) >> 4][((attrib & 0x0f) * 1024) + 0x03f1 + num * 4];
-            _Blue = m_vram[(attrib & 0xf0) >> 4][((attrib & 0x0f) * 1024) + 0x03f2 + num * 4];
-            //return _Red * 65536 + _Green * 256 + _Blue;
-            return (_Red << 16) | (_Green << 8) | _Blue;
-            //return System.Drawing.Color.FromArgb(255, _Red, _Green, _Blue).ToArgb();
+            // Alex M: original:
+            ////flash = 1 = on
+            ////pix = 1 = on
+            ////int red, green, blue;
+            ////byte num = (byte)(flash * 2 + pix);
+            ////var vramF0 = m_vram[(attrib & 0xf0) >> 4];
+            ////var index = ((attrib & 0x0f) * 1024) + 0x03f0 + num * 4;
+            ////red = vramF0[index];
+            ////green = vramF0[index + 1];
+            ////blue = vramF0[index + 2];
+            //////return _Red * 65536 + _Green * 256 + _Blue;
+            ////return (red << 16) | (green << 8) | blue;
+            //////return System.Drawing.Color.FromArgb(255, _Red, _Green, _Blue).ToArgb();
+
+            // WARNING: pix/flash range 0 or 1
+            var num = (flash << 1) | pix;
+            //byte num = (byte)((flash << 1) + pix);
+            var palvram = m_vram[(attrib & 0xf0) >> 4];
+            var index = ((attrib & 0x0f) << 10) + 0x03f0 + (num << 2);
+            var red =   palvram[index];
+            var green = palvram[index+1];
+            var blue =  palvram[index+2];
+            return (red << 16) | (green << 8) | blue;
         }
 
         /*private int ToColor(int color)
@@ -90,16 +103,24 @@ namespace ZXMAK2.Hardware.Sprinter
         */
         private int ColorPaletteToRGB(int color, int palette)
         {
-            int red, green, blue;
+            // Alex M: original
+            //int red, green, blue;
+            //var index1 = (color & 0xf0) >> 4;
+            //var index2 = ((color & 0x0f) * 1024) + 0x03e0 + palette * 4;
+            //red = m_vram[index1][index2];
+            //green = m_vram[index1][index2+1];
+            //blue = m_vram[index1][index2+2];
+            //return (red << 16) | (green << 8) | blue;
+            ////return _Red * 65536 + _Green * 256 + _Blue;
+            ////return System.Drawing.Color.FromArgb(255, _Red, _Green, _Blue).ToArgb();
 
             var index1 = (color & 0xf0) >> 4;
-            var index2 = ((color & 0x0f) * 1024) + 0x03e0 + palette * 4;
-            red = m_vram[index1][index2];
-            green = m_vram[index1][index2+1];
-            blue = m_vram[index1][index2+2];
+            var index2 = ((color & 0x0f) << 10) + 0x03e0 + (palette << 2);
+            var palvram = m_vram[index1];
+            var red = palvram[index2];
+            var green = palvram[index2 + 1];
+            var blue = palvram[index2 + 2];
             return (red << 16) | (green << 8) | blue;
-            //return _Red * 65536 + _Green * 256 + _Blue;
-            //return System.Drawing.Color.FromArgb(255, _Red, _Green, _Blue).ToArgb();
         }
 
         protected override void EndFrame()
@@ -112,17 +133,21 @@ namespace ZXMAK2.Hardware.Sprinter
             int num = 0;
             byte linenum, linenum1;
             byte mode0, mode1, mode2, mode10, mode11, mode12, mode, scrbyte, attrbyte;//, scrbyte1, attrbyte1;
-            for (int y = 0; y < 32; y++)
+            var cmr0bit3 = (Memory.CMR0 & 8) >> 3;// (Memory.CMR0 & 8) == 0 ? 0 : 1;
+            var flashState = _flashState == 0 ? 0 : 1;
+            for (var y = 0; y < 32; y++)
             {
-                for (int y1 = 0; y1 < 8; y1++)
+                for (var y1 = 0; y1 < 8; y1++)
                 {
-                    for (int x = 0; x < 40; x++)
+                    for (var x = 0; x < 40; x++)
                     {
-                        linenum = (byte)(1 + 2 * x + ((m_mode & 1) == 1 ? 128 : 0));
+                        linenum = (byte)(1 + (x << 1) + ((m_mode & 1) << 7));//((m_mode & 1) == 1 ? 128 : 0));
                         byte vpage = (byte)((linenum & 0xF0) >> 4);
-                        mode0 = m_vram[vpage][((linenum & 0x0f) * 1024) + 0x0300 + 4 * y];
-                        mode1 = m_vram[vpage][((linenum & 0x0f) * 1024) + 0x0301 + 4 * y];
-                        mode2 = m_vram[vpage][((linenum & 0x0f) * 1024) + 0x0302 + 4 * y];
+                        var vrampage = m_vram[vpage];
+                        var vrampageIndex = ((linenum & 0x0f) << 10) + 0x0300 + (y << 2);
+                        mode0 = vrampage[vrampageIndex];
+                        mode1 = vrampage[vrampageIndex+1];
+                        mode2 = vrampage[vrampageIndex+2];
 
                         mode = (byte)((mode0 & 0x30) >> 4);
                         switch (mode)
@@ -133,11 +158,12 @@ namespace ZXMAK2.Hardware.Sprinter
                                     int bloknum = ((mode0 & 15) << 1) | ((mode1 & 4) >> 2);
                                     int palette = (mode0 & 192) >> 6;
                                     int ln = (mode1 & 0xf0) >> 4;
-                                    int col = (((mode1 & 0x08) | y1) * 1024) + (bloknum * 32) + ((mode1 & 3) << 3);
-                                    for (int i = 0; i < 8; i++)
+                                    int col = (((mode1 & 0x08) | y1) << 10) + (bloknum << 5) + ((mode1 & 3) << 3);
+                                    var palvram = m_vram[ln];
+                                    for (var i = 0; i < 8; i++)
                                     {
-                                        videoBuffer[num++] = ColorPaletteToRGB(((m_vram[ln][col + i] & 0xf0)>>4), palette);
-                                        videoBuffer[num++] = ColorPaletteToRGB((m_vram[ln][col + i] & 0x0f), palette);
+                                        videoBuffer[num++] = ColorPaletteToRGB((palvram[col + i] & 0xf0)>>4, palette);
+                                        videoBuffer[num++] = ColorPaletteToRGB((palvram[col + i] & 0x0f), palette);
                                         //                                        scrbyte = m_vram[ln][col];
                                     }
 
@@ -148,15 +174,18 @@ namespace ZXMAK2.Hardware.Sprinter
                                 {
                                     linenum1 = (byte)(linenum + 1);
                                     vpage = (byte)((linenum1 & 0xF0) >> 4);
-                                    mode10 = m_vram[vpage][((linenum1 & 0x0f) * 1024) + 0x0300 + 4 * y];
-                                    mode11 = m_vram[vpage][((linenum1 & 0x0f) * 1024) + 0x0301 + 4 * y];
-                                    mode12 = m_vram[vpage][((linenum1 & 0x0f) * 1024) + 0x0302 + 4 * y];
+                                    vrampage = m_vram[vpage];
+                                    vrampageIndex = ((linenum1 & 0x0f) << 10) + 0x0300 + (y << 2);
+                                    mode10 = vrampage[vrampageIndex];
+                                    mode11 = vrampage[vrampageIndex+1];
+                                    mode12 = vrampage[vrampageIndex+2];
 
-                                    int bloknum = ((mode0 & 15) << 1) | ((this.Memory.CMR0 & 8) == 0 ? 0 : 1);
+                                    int bloknum = ((mode0 & 15) << 1) | cmr0bit3;
                                     //ushort addr = (ushort)(((mode0 & 192) << 5) | (mode1) | (y1 << 8));
-                                    scrbyte = m_vram[((mode1 & 0xf0) >> 4)][((mode1 & 0x0f) * 1024) + (bloknum * 32) + ((mode0 & 0xC0) >> 3) + y1];
-                                    attrbyte = m_vram[((mode2 & 0xf0) >> 4)][((mode2 & 0x0f) * 1024) + (bloknum * 32) + ((mode0 & 0xC0) >> 5) + 24];
-                                    for (int i = 0, msk = 128; i < 8; i++)
+                                    scrbyte = m_vram[((mode1 & 0xf0) >> 4)][((mode1 & 0x0f) << 10) + (bloknum << 5) + ((mode0 & 0xC0) >> 3) + y1];
+                                    attrbyte = m_vram[((mode2 & 0xf0) >> 4)][((mode2 & 0x0f) << 10) + (bloknum << 5) + ((mode0 & 0xC0) >> 5) + 24];
+                                    //for (int i = 0, msk=128; i < 8; i++, msk >>= 1)
+                                    for (var i = 7; i >= 0; i--)
                                     {
                                         /*if ((scrbyte & msk) == 0)
                                         {
@@ -166,13 +195,16 @@ namespace ZXMAK2.Hardware.Sprinter
                                         {
                                             videoBuffer[num++] = ToColor(attrbyte & 0x07);
                                         }*/
-                                        videoBuffer[num++] = TXTPaletteToRGB(attrbyte, ((scrbyte & msk) == 0) ? 0 : 1, (this._flashState == 0) ? 0 : 1);
-                                        msk = msk >> 1;
+                                        videoBuffer[num++] = TXTPaletteToRGB(
+                                            attrbyte,
+                                            (scrbyte >> i) & 1, //(scrbyte & msk) == 0 ? 0 : 1, 
+                                            flashState);
                                     }
-                                    bloknum = ((mode10 & 15) << 1) | ((this.Memory.CMR0 & 8) == 0 ? 0 : 1);
-                                    scrbyte = m_vram[((mode11 & 0xf0) >> 4)][((mode11 & 0x0f) * 1024) + (bloknum * 32) + ((mode10 & 0xC0) >> 3) + y1];
-                                    attrbyte = m_vram[((mode12 & 0xf0) >> 4)][((mode12 & 0x0f) * 1024) + (bloknum * 32) + ((mode10 & 0xC0) >> 5) + 24];
-                                    for (int i = 0, msk = 128; i < 8; i++)
+                                    bloknum = ((mode10 & 15) << 1) | cmr0bit3;
+                                    scrbyte = m_vram[((mode11 & 0xf0) >> 4)][((mode11 & 0x0f) << 10) + (bloknum << 5) + ((mode10 & 0xC0) >> 3) + y1];
+                                    attrbyte = m_vram[((mode12 & 0xf0) >> 4)][((mode12 & 0x0f) << 10) + (bloknum << 5) + ((mode10 & 0xC0) >> 5) + 24];
+                                    //for (int i = 0, msk=128; i < 8; i++, msk >>= 1)
+                                    for (var i = 7; i >= 0; i--)
                                     {
 /*                                        if ((scrbyte & msk) == 0)
                                         {
@@ -183,8 +215,10 @@ namespace ZXMAK2.Hardware.Sprinter
                                             videoBuffer[num++] = ToColor(attrbyte & 0x07);
                                         }*/
 
-                                        videoBuffer[num++] = TXTPaletteToRGB(attrbyte, ((scrbyte & msk) == 0) ? 0 : 1, (this._flashState==0)?0:1);
-                                        msk = msk >> 1;
+                                        videoBuffer[num++] = TXTPaletteToRGB(
+                                            attrbyte, 
+                                            (scrbyte >> i) & 1, //(scrbyte & msk) == 0 ? 0 : 1, 
+                                            flashState);
                                     }
                                 }
                                 break;
@@ -194,11 +228,12 @@ namespace ZXMAK2.Hardware.Sprinter
                                     int bloknum = ((mode0 & 15) << 1) | ((mode1 & 4) >> 2);
                                     int palette = (mode0 & 192) >> 6;
                                     int ln = (mode1&0xf0)>>4;
-                                    int col = (((mode1 & 0x08) | y1) * 1024) + (bloknum * 32) + ((mode1 & 3) << 3);
-                                    for (int i = 0; i < 8; i++)
+                                    int col = (((mode1 & 0x08) | y1) << 10) + (bloknum << 5) + ((mode1 & 3) << 3);
+                                    var palvram = m_vram[ln];
+                                    for (var i = 0; i < 8; i++)
                                     {
-                                        videoBuffer[num++] = ColorPaletteToRGB(m_vram[ln][col + i], palette);
-                                        videoBuffer[num++] = ColorPaletteToRGB(m_vram[ln][col + i], palette);
+                                        videoBuffer[num++] = ColorPaletteToRGB(palvram[col + i], palette);
+                                        videoBuffer[num++] = ColorPaletteToRGB(palvram[col + i], palette);
 //                                        scrbyte = m_vram[ln][col];
                                     }
                                 }
@@ -206,14 +241,18 @@ namespace ZXMAK2.Hardware.Sprinter
                             //спектрумовский, 40симв
                             case 3:
                                 {
-                                    int bloknum = ((mode0 & 15) << 1) | ((this.Memory.CMR0 & 8)==0?0:1);
+                                    int bloknum = ((mode0 & 15) << 1) | cmr0bit3;
                                     //ushort addr = (ushort)(((mode0 & 192) << 5) | (mode1) | (y1 << 8));
-                                    scrbyte = m_vram[((mode1 & 0xf0) >> 4)][((mode1 & 0x0f) * 1024) + (bloknum * 32) + ((mode0 & 0xC0) >> 3) + y1];
-                                    attrbyte = m_vram[((mode2 & 0xf0) >> 4)][((mode2 & 0x0f) * 1024) + (bloknum * 32) + ((mode0 & 0xC0) >> 5) + 24];
-                                    for (int i = 0, msk=128; i < 8; i++)
+                                    scrbyte = m_vram[((mode1 & 0xf0) >> 4)][((mode1 & 0x0f) << 10) + (bloknum << 5) + ((mode0 & 0xC0) >> 3) + y1];
+                                    attrbyte = m_vram[((mode2 & 0xf0) >> 4)][((mode2 & 0x0f) << 10) + (bloknum << 5) + ((mode0 & 0xC0) >> 5) + 24];
+                                    //for (int i = 0, msk=128; i < 8; i++, msk >>= 1)
+                                    for (var i = 7; i >= 0; i--)
                                     {
                                         //надо проверять, возможно не правильно формируется цвет, надо 16 цветов или целый байт атрибута учавствует в выборке из палитры?
-                                        videoBuffer[num++] = TXTPaletteToRGB(attrbyte, ((scrbyte & msk) == 0) ? 0 : 1, (this._flashState == 0) ? 0 : 1);
+                                        videoBuffer[num++] = TXTPaletteToRGB(
+                                            attrbyte,
+                                            (scrbyte >> i) & 1,//(scrbyte & msk) == 0 ? 0 : 1, 
+                                            flashState);
 /*                                        if ((scrbyte & msk) == 0)
                                         {
                                             videoBuffer[num++] = ToColor( (attrbyte & 0x38) >> 3);
@@ -224,7 +263,6 @@ namespace ZXMAK2.Hardware.Sprinter
                                             videoBuffer[num++] = ToColor(attrbyte & 0x07);
                                             videoBuffer[num++] = ToColor(attrbyte & 0x07);
                                         }*/
-                                        msk = msk >> 1;
                                     }
                                 }
                                 break;
@@ -253,38 +291,20 @@ namespace ZXMAK2.Hardware.Sprinter
 
         public virtual byte RGADR
         {
-            get
-            {
-                return this.m_rgadr;
-            }
-            set
-            {
-                this.m_rgadr = value;
-            }
+            get { return m_rgadr; }
+            set { m_rgadr = value; }
         }
 
         public virtual byte RGMOD
         {
-            get
-            {
-                return this.m_mode;
-            }
-            set
-            {
-                this.m_mode = value;
-            }
+            get { return m_mode; }
+            set { m_mode = value; }
         }
 
         public virtual byte[][]VRAM 
         {
-            get
-            {
-                return this.m_vram;
-            }
-            set
-            {
-                this.m_vram = value;
-            }
+            get { return m_vram; }
+            set { m_vram = value; }
         }
 
         public void UpdateFrame()
