c0324b5d7326dbe6e86bd7a4aee05652d432fd52
Alex Makeev <zxmak@mail.ru>
2015-07-01 05:35:18 +0000

    rename FormBreakpoints

---
+++ src/ZXMAK2.Hardware.WinForms/General/Views/FormBreakpoints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace ZXMAK2.Hardware.WinForms.General.Views
+{
+    public partial class FormBreakpoints : DockContent
+    {
+        public FormBreakpoints()
+        {
+            InitializeComponent();
+        }
+    }
+}
