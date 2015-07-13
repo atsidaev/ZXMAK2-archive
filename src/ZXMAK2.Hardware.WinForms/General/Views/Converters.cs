96c1fd182ee082f19941c4e08f04ac6ed140b0bf
Alex Makeev <zxmak@mail.ru>
2015-07-13 00:38:08 +0000

    move RegPairToString converter into Converters

---
+++ src/ZXMAK2.Hardware.WinForms/General/Views/Converters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZXMAK2.Mvvm;
+
+namespace ZXMAK2.Hardware.WinForms.General.Views
+{
+    public static class Converters
+    {
+        public static readonly IValueConverter RegPairToString = 
+            new IntegerToStringConverter() { IsHex = true, DigitCount = 4, };
+    }
+}
