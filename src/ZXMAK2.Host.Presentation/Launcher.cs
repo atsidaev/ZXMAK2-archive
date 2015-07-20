448c001c41875fe243caf5330726cdc7776ca8fb
Alex Makeev <zxmak@mail.ru>
2015-07-20 08:52:34 +0000

    fix fileNames

--- src/ZXMAK2.Host.Presentation/Launcher.cs
+++ src/ZXMAK2.Host.Presentation/Launcher.cs
@@ -39,15 +39,15 @@ namespace ZXMAK2.Host.Presentation
                     var list = new List<Argument>();
                     list.Add(new Argument("view", view));
                     list.Add(new Argument("args", args));
-                    using (var presenter = m_resolver.Resolve<IMainViewModel>(list.ToArray()))
+                    using (var viewModel = m_resolver.Resolve<IMainViewModel>(list.ToArray()))
                     {
                         var synchronizeInvoke = view as ISynchronizeInvoke;
                         if (synchronizeInvoke != null)
                         {
-                            presenter.Attach(synchronizeInvoke);
+                            viewModel.Attach(synchronizeInvoke);
                         }
-                        view.DataContext = presenter;
-                        presenter.Run();
+                        view.DataContext = viewModel;
+                        viewModel.Run();
                     }
                 }
             }
