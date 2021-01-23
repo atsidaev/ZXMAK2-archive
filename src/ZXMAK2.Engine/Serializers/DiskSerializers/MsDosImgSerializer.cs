0dc01b6f517f4bb5c54ade550b51d93f3d071424
Alexander Tsidaev <a.tsidaev@gmail.com>
2021-01-24 02:52:34 +0500

    MSDOS FAT12 Disk serializer
    Only 5.25" 720k floppies are supported (8 sectors per track)

---
+++ src/ZXMAK2.Engine/Serializers/DiskSerializers/MsDosImgSerializer.cs
@@ -0,0 +1,33 @@
+﻿// IMG serializer
+// Author: zx.pk.ru\Дмитрий
+using ZXMAK2.Model.Disk;
+
+
+namespace ZXMAK2.Serializers.DiskSerializers
+{
+    /// <summary>
+    /// SPRINTER disk image
+    /// </summary>
+    public class MsDosImgSerializer : SectorImageSerializerBase
+    {
+        public MsDosImgSerializer(DiskImage diskImage)
+            : base(diskImage)
+        {
+        }
+        
+        public override string FormatExtension
+        {
+            get { return "IMG"; }
+        }
+        
+        protected override int SectorSizeCode
+        {
+            get { return 2; }
+        }
+
+        protected override int[] GetSectorMap(int cyl, int head)
+        {
+            return new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        }
+    }
+}
